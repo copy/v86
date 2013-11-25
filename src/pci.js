2dabbe710d4c2d081ccfea2b12671e891681bd10
copy <copy@copy.sh>
2013-11-25 12:43:39 +0100

    Improved PCI, added some experimental devices

--- src/pci.js
+++ src/pci.js
@@ -13,16 +13,23 @@ var
 /** @const */ ,PCI_INTERRUPT_LINE =	0x3c	/* 8 bits */
 /** @const */ ,PCI_CLASS_REVISION =	0x08;	/* High 24 bits are class, low 8 revision */
 
+var 
+/** @const */ PCI_CONFIG_ADDRESS = 0xCF8,
+/** @const */ PCI_CONFIG_DATA = 0xCFC;
+
 /** @constructor */
 function PCI(dev)
 {
     var
         io = dev.io,
-        pci_data = 0,
-        pci_counter = 0,
-        pci_response = -1,
-        pci_status = -1,
-        self = this;
+        pci_data = new Uint8Array(4),
+        pci_response = new Uint8Array(4),
+        pci_status = new Uint8Array(4),
+
+        pci_data32 = new Int32Array(pci_data.buffer),
+        pci_response32 = new Int32Array(pci_response.buffer),
+        pci_status32 = new Int32Array(pci_status.buffer),
+        pci = this;
 
     // TODO: Change the format of this
     this.devices = {};
@@ -39,135 +46,119 @@ function PCI(dev)
         }
     });*/
 
-    io.register_read(0xCFC, function()
+    io.register_read(PCI_CONFIG_DATA, function()
     {
-        return pci_response & 0xFF;
+        return pci_response[0];
+    });
+    io.register_read(PCI_CONFIG_DATA | 1, function()
+    {
+        return pci_response[1];
+    });
+    io.register_read(PCI_CONFIG_DATA | 2, function()
+    {
+        return pci_response[2];
+    });
+    io.register_read(PCI_CONFIG_DATA | 3, function()
+    {
+        return pci_response[3];
     });
 
-    io.register_read(0xCFD, function()
+    io.register_read(PCI_CONFIG_ADDRESS, function()
     {
-        return pci_response >> 8 & 0xFF;
+        return pci_status[0];
     });
-    io.register_read(0xCFE, function()
+    io.register_read(PCI_CONFIG_ADDRESS | 1, function()
     {
-        return pci_response >> 16 & 0xFF;
+        return pci_status[1];
     });
-    io.register_read(0xCFF, function()
+    io.register_read(PCI_CONFIG_ADDRESS | 2, function()
     {
-        return pci_response >> 24 & 0xFF;
+        return pci_status[2];
     });
-
-    io.register_read(0xCF8, function()
+    io.register_read(PCI_CONFIG_ADDRESS | 3, function()
     {
-        return pci_status & 0xFF;
-    });
-    io.register_read(0xCF9, function()
-    {
-        return pci_status >> 8 & 0xFF;
-    });
-    io.register_read(0xCFA, function()
-    {
-        return pci_status >> 16 & 0xFF;
-    });
-    io.register_read(0xCFB, function()
-    {
-        return pci_status >> 24 & 0xFF;
+        return pci_status[3];
     });
 
-    io.register_write(0xCF8, function(out_byte)
+    io.register_write(PCI_CONFIG_ADDRESS, function(out_byte)
     {
-        pci_data = pci_data & ~0xFF | out_byte;
+        pci_data[0] = out_byte;
     });
-    io.register_write(0xCF9, function(out_byte)
+    io.register_write(PCI_CONFIG_ADDRESS | 1, function(out_byte)
     {
-        pci_data = pci_data & ~0xFF00 | out_byte << 8;
+        pci_data[1] = out_byte;
     });
-    io.register_write(0xCFA, function(out_byte)
+    io.register_write(PCI_CONFIG_ADDRESS | 2, function(out_byte)
     {
-        pci_data = pci_data & ~0xFF0000 | out_byte << 16;
+        pci_data[2] = out_byte;
     });
-    io.register_write(0xCFB, function(out_byte)
+    io.register_write(PCI_CONFIG_ADDRESS | 3, function(out_byte)
     {
-        pci_data = pci_data & 0xFFFFFF | out_byte << 24;
-        pci_query(pci_data);
+        pci_data[3] = out_byte;
+
+        pci_query();
     });
 
-    function pci_query(dword)
+    function pci_query()
     {
         var dbg_line = "PCI: ";
         
         // Bit | .31                     .0
         // Fmt | EBBBBBBBBDDDDDFFFRRRRRR00
 
-        var bdf = (dword & 0x7FFFFFFF) >> 8,
-            addr = dword & 0xFC,
+        var bdf = pci_data[2] << 8 | pci_data[1],
+            addr = pci_data[0] & 0xFC,
             devfn = bdf & 0xFF,
             bus = bdf >> 8,
             dev = bdf >> 3 & 0x1F,
             fn = bdf & 7,
-            enabled = dword >> 31 & 1;
+            enabled = pci_data[3] >> 7;
 
         dbg_line += " enabled=" + (enabled);
-        dbg_line += " bdf=" + h(bdf);
-        dbg_line += " addr=" + h(addr);
+        dbg_line += " bdf=" + h(bdf, 4);
+        dbg_line += " addr=" + h(addr, 2);
 
-        dbg_log(dbg_line + " " + h(dword >>> 0, 8), LOG_PCI);
+        dbg_log(dbg_line + " " + h(pci_data32[0] >>> 0, 8), LOG_PCI);
 
-        if(dword === (0x80000000 | 0))
+        var device = pci.devices[bdf];
+
+        if(device !== undefined)
         {
-            pci_status = 0x80000000;
-        }
-        else if(self.devices[bdf])
-        {
-            var device = self.devices[bdf];
-
-            pci_status = 0x80000000;
-
-            if(addr === PCI_VENDOR_ID)
-            {
-                pci_response = device.vendor_id;
-            }
-            else if(addr === PCI_CLASS_REVISION)
-            {
-                pci_response = device.class_revision;
-            }
-            else if(addr === PCI_BASE_ADDRESS_5)
-            {
-                pci_response = device.iobase;
-            }
-            else if(addr === PCI_INTERRUPT_LINE)
-            {
-                pci_response = device.irq;
-            }
-            else
-            {
-                dbg_log("unimplemented addr " + h(addr) + " for device " + h(bdf), LOG_PCI);
-                pci_response = 0;
-            }
+            pci_status32[0] = 0x80000000 | 0;
+            pci_response32[0] = device[addr >> 2];
         }
         else
         {
-            pci_response = 0;
-            pci_status = 0;
+            pci_response32[0] = 0;
+            pci_status32[0] = 0;
         }
     }
 
     this.register_device = function(device, device_id)
     {
-        dbg_assert(!this.devices[device_id]);
-        this.devices[device_id] = device;
+        dbg_assert(!pci.devices[device_id]);
+        dbg_assert(device.length === 64);
+
+        // convert bytewise notation from lspci to double words
+        pci.devices[device_id] = new Int32Array(new Uint8Array(device).buffer);
     };
 
-    // ~% lspci -x   
+    // Some experimental PCI devices taken from my PC:
+
     // 00:00.0 Host bridge: Intel Corporation 4 Series Chipset DRAM Controller (rev 02)
-    // 00: 86 80 20 2e 06 00 90 20 02 00 00 06 00 00 00 00
-    // 10: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
-    // 20: 00 00 00 00 00 00 00 00 00 00 00 00 43 10 d3 82
-    // 30: 00 00 00 00 e0 00 00 00 00 00 00 00 00 00 00 00
-    this.register_device({
-        irq: 0,
-        iobase: 0,
-        vendor_id: 0x8680202e,
-        class_revision: 0x06009020,
-    }, 0);
+    this.register_device([
+        0x86, 0x80, 0x20, 0x2e, 0x06, 0x00, 0x90, 0x20, 0x02, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x43, 0x10, 0xd3, 0x82,
+        0x00, 0x00, 0x00, 0x00, 0xe0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    ], 0);
+
+    // 00:1e.0 PCI bridge: Intel Corporation 82801 PCI Bridge (rev 90)
+    this.register_device([
+        0x86, 0x80, 0x4e, 0x24, 0x07, 0x01, 0x10, 0x00, 0x90, 0x01, 0x04, 0x06, 0x00, 0x00, 0x01, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x05, 0x20, 0xe0, 0xe0, 0x80, 0x22,
+        0xb0, 0xfe, 0xb0, 0xfe, 0xf1, 0xff, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x50, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x00, 0x02, 0x00,
+    ], 0x1e << 3);
+
 }
