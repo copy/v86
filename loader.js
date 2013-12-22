f2aea875d5a392c3b10010476674e5dda0deb5c2
copy <copy@copy.sh>
2013-12-22 22:20:57 +0100

    Sync load scripts for debug mode

--- loader.js
+++ loader.js
@@ -6,23 +6,26 @@
         CORE_FILES="const.js io.js cpu.js main.js ide.js pci.js floppy.js memory.js dma.js pit.js vga.js ps2.js pic.js rtc.js uart.js hpet.js"
         BROWSER_FILES="browser/main.js browser/screen.js browser/keyboard.js browser/mouse.js"
 
-    window.onload = function()
+    load_scripts(CORE_FILES);
+    load_scripts(BROWSER_FILES);
+
+    function load_scripts(resp)
     {
-        load_scripts(CORE_FILES);
-        load_scripts(BROWSER_FILES);
+        var files = resp.split(" "),
+            script;
 
-        function load_scripts(resp)
+        for(var i = 0; i < files.length; i++)
         {
-            var files = resp.split(" "),
-                script;
+            // this may be a bad idea, if someone tries to 
+            // load this script after the document has loaded,
+            // but it's necessary to ensure that scripts are 
+            // loaded in order
+            document.write('<script src="' + PATH + files[i] + "?" + Math.random() + '"></script>');
 
-            for(var i = 0; i < files.length; i++)
-            {
-                script = document.createElement("script");
-                script.src = PATH + files[i] + "?" + Math.random();
-                document.body.appendChild(script);
-            }
+            //script = document.createElement("script");
+            //script.src = PATH + files[i] + "?" + Math.random();
+            //script.defer = "defer";
+            //document.body.appendChild(script);
         }
-    };
-
+    }
 })();
