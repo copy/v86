fe649190431d5e6c53e75cfc1187f839e15fada6
copy <copy@copy.sh>
2014-07-09 02:25:37 +0200

    Fix PS/2 irq on port data

--- src/ps2.js
+++ src/ps2.js
@@ -275,7 +275,7 @@ function PS2(cpu, keyboard, mouse)
             last_port60_byte = mouse_buffer.shift();
             dbg_log("Port 60 read (mouse): " + h(last_port60_byte), LOG_PS2);
 
-            if(mouse_buffer.length > 1)
+            if(mouse_buffer.length >= 1)
             {
                 mouse_irq();
             }
@@ -285,7 +285,7 @@ function PS2(cpu, keyboard, mouse)
             last_port60_byte = kbd_buffer.shift();
             dbg_log("Port 60 read (kbd)  : " + h(last_port60_byte), LOG_PS2);
 
-            if(kbd_buffer.length > 1)
+            if(kbd_buffer.length >= 1)
             {
                 kbd_irq();
             }
