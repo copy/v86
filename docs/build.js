218da431f86856b27d7dc11eb8b738d0b5ecb2b7
copy <copy@copy.sh>
2015-01-11 18:49:08 +0100

    Documentation build script

---
+++ docs/build.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+var markdox = require("markdox");
+
+var options = {
+    output: __dirname + "/api.md", 
+    template: __dirname + "/template.md.ejs"
+};
+
+var files = [
+    __dirname + "/../src/browser/starter.js",
+];
+
+markdox.process(files, options, function() {
+  console.log("Ok.");
+});
