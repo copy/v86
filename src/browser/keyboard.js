54fbc5c66af28387b9cec11d110246cc809042bd
copy <copy@copy.sh>
2015-05-17 02:49:34 +0200

    Keyboard handling using new event.code. #31

--- src/browser/keyboard.js
+++ src/browser/keyboard.js
@@ -7,7 +7,7 @@
  */
 function KeyboardAdapter(bus)
 {
-    var 
+    var
         /**
          * @type {!Object.<boolean>}
          */
@@ -15,16 +15,16 @@ function KeyboardAdapter(bus)
 
         keyboard = this;
 
-    /** 
+    /**
      * Set by emulator
      * @type {boolean}
      */
     this.emu_enabled = true;
 
-    /** 
+    /**
      * Format:
      * Javascript event.keyCode -> make code
-     * @const 
+     * @const
      */
     var charmap = new Uint16Array([
         0, 0, 0, 0,  0, 0, 0, 0,
@@ -42,17 +42,17 @@ function KeyboardAdapter(bus)
         0x50, 0, 0, 0,  0, 0x52, 0x53, 0,
 
         // 0x30: numbers
-        0x0B, 0x02, 0x03, 0x04,  0x05, 0x06, 0x07, 0x08, 
+        0x0B, 0x02, 0x03, 0x04,  0x05, 0x06, 0x07, 0x08,
         0x09, 0x0A,
 
         // 0x3B: ;= (firefox only)
-        0, 0x27, 0, 0x0D, 0, 0, 
-            
+        0, 0x27, 0, 0x0D, 0, 0,
+
         // 0x40
         0,
 
         // 0x41: letters
-        0x1E, 0x30, 0x2E, 0x20, 0x12, 0x21, 0x22, 0x23, 0x17, 0x24, 0x25, 0x26, 0x32, 
+        0x1E, 0x30, 0x2E, 0x20, 0x12, 0x21, 0x22, 0x23, 0x17, 0x24, 0x25, 0x26, 0x32,
         0x31, 0x18, 0x19, 0x10, 0x13, 0x1F, 0x14, 0x16, 0x2F, 0x11, 0x2D, 0x15, 0x2C,
 
         // 0x5B: Left Win, Right Win, Menu
@@ -83,7 +83,7 @@ function KeyboardAdapter(bus)
         // ,
         0, 0, 0, 0,  0, 0, 0, 0,
         0, 0, 0x27, 0x0D,  0x33, 0x0C, 0x34, 0x35,
-        
+
         // 0xC0
         // `
         0x29, 0, 0, 0,  0, 0, 0, 0,
@@ -100,9 +100,124 @@ function KeyboardAdapter(bus)
         0, 0, 0, 0,            0, 0, 0, 0,
     ]);
 
+    // From:
+    // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code#Code_values_on_Linux_%28X11%29_%28When_scancode_is_available%29
+    // http://stanislavs.org/helppc/make_codes.html
+    // http://www.computer-engineering.org/ps2keyboard/scancodes1.html
+    //
+    // Mapping from event.code to scancode
+    var codemap = {
+        "Escape": 0x0001,
+        "Digit1": 0x0002,
+        "Digit2": 0x0003,
+        "Digit3": 0x0004,
+        "Digit4": 0x0005,
+        "Digit5": 0x0006,
+        "Digit6": 0x0007,
+        "Digit7": 0x0008,
+        "Digit8": 0x0009,
+        "Digit9": 0x000a,
+        "Digit0": 0x000b,
+        "Minus": 0x000c,
+        "Equal": 0x000d,
+        "Backspace": 0x000e,
+        "Tab": 0x000f,
+        "KeyQ": 0x0010,
+        "KeyW": 0x0011,
+        "KeyE": 0x0012,
+        "KeyR": 0x0013,
+        "KeyT": 0x0014,
+        "KeyY": 0x0015,
+        "KeyU": 0x0016,
+        "KeyI": 0x0017,
+        "KeyO": 0x0018,
+        "KeyP": 0x0019,
+        "BracketLeft": 0x001a,
+        "BracketRight": 0x001b,
+        "Enter": 0x001c,
+        "ControlLeft": 0x001d,
+        "KeyA": 0x001e,
+        "KeyS": 0x001f,
+        "KeyD": 0x0020,
+        "KeyF": 0x0021,
+        "KeyG": 0x0022,
+        "KeyH": 0x0023,
+        "KeyJ": 0x0024,
+        "KeyK": 0x0025,
+        "KeyL": 0x0026,
+        "Semicolon": 0x0027,
+        "Quote": 0x0028,
+        "Backquote": 0x0029,
+        "ShiftLeft": 0x002a,
+        "Backslash": 0x002b,
+        "KeyZ": 0x002c,
+        "KeyX": 0x002d,
+        "KeyC": 0x002e,
+        "KeyV": 0x002f,
+        "KeyB": 0x0030,
+        "KeyN": 0x0031,
+        "KeyM": 0x0032,
+        "Comma": 0x0033,
+        "Period": 0x0034,
+        "Slash": 0x0035,
+        "ShiftRight": 0x0036,
+        "NumpadMultiply": 0x0037,
+        "AltLeft": 0x0038,
+        "Space": 0x0039,
+        "CapsLock": 0x003a,
+        "F1": 0x003b,
+        "F2": 0x003c,
+        "F3": 0x003d,
+        "F4": 0x003e,
+        "F5": 0x003f,
+        "F6": 0x0040,
+        "F7": 0x0041,
+        "F8": 0x0042,
+        "F9": 0x0043,
+        "F10": 0x0044,
+        "NumLock": 0x0045,
+        "ScrollLock": 0x0046,
+        "Numpad7": 0x0047,
+        "Numpad8": 0x0048,
+        "Numpad9": 0x0049,
+        "NumpadSubtract": 0x004a,
+        "Numpad4": 0x004b,
+        "Numpad5": 0x004c,
+        "Numpad6": 0x004d,
+        "NumpadAdd": 0x004e,
+        "Numpad1": 0x004f,
+        "Numpad2": 0x0050,
+        "Numpad3": 0x0051,
+        "Numpad0": 0x0052,
+        "NumpadDecimal": 0x0053,
+        "IntlBackslash": 0x0056,
+        "F11": 0x0057,
+        "F12": 0x0058,
+
+        "NumpadEnter": 0xe01c,
+        "ControlRight": 0xe01d,
+        "NumpadDivide": 0xe035,
+        //"PrintScreen": 0x0063,
+        "AltRight": 0xe038,
+        "Home": 0xe04f,
+        "ArrowUp": 0xe048,
+        "PageUp": 0xe049,
+        "ArrowLeft": 0xe04b,
+        "ArrowRight": 0xe04d,
+        "End": 0xe04f,
+        "ArrowDown": 0xe050,
+        "PageDown": 0xe051,
+        "Insert": 0xe052,
+        "Delete": 0xe053,
+
+        "OSLeft": 0xe05b,
+        "OSRight": 0xe05c,
+        "ContextMenu": 0xe05d,
+    };
+
     this.bus = bus;
 
-    this.destroy = function() 
+    this.destroy = function()
     {
         window.removeEventListener("keyup", keyup_handler, false);
         window.removeEventListener("keydown", keydown_handler, false);
@@ -126,7 +241,7 @@ function KeyboardAdapter(bus)
 
     function may_handle(e)
     {
-        if(e.shiftKey && e.ctrlKey && e.keyCode === 74) 
+        if(e.shiftKey && e.ctrlKey && e.keyCode === 74)
         {
               // don't prevent opening chromium dev tools
               // maybe add other important combinations here, too
@@ -150,50 +265,29 @@ function KeyboardAdapter(bus)
         }
     }
 
+    function translate(e)
+    {
+        if(e.code !== undefined)
+        {
+            var code = codemap[e.code];
+
+            if(code !== undefined)
+            {
+                return code;
+            }
+        }
+
+        return charmap[e.keyCode];
+    }
+
     function keyup_handler(e)
     {
-        if(!may_handle(e))
-        {
-            return;
-        }
-
-        var code = e.keyCode;
-
-        if(!keys_pressed[code])
-        {
-            // stray keyup
-            return false;
-        }
-
-        keys_pressed[code] = false;
-
-        if(!handler(code, false))
-        {
-            e.preventDefault();
-        }
+        return handler(e, false);
     }
 
     function keydown_handler(e)
     {
-        if(!may_handle(e))
-        {
-            return;
-        }
-
-        var code = e.keyCode;
-
-        if(keys_pressed[code])
-        {
-            handler(code, false);
-        }
-
-        keys_pressed[code] = true;
-
-
-        if(!handler(code, true))
-        {
-            e.preventDefault();
-        }
+        return handler(e, true);
     }
 
     function blur_handler(e)
@@ -208,7 +302,7 @@ function KeyboardAdapter(bus)
 
             if(keys_pressed[key])
             {
-                handler(key, false);
+                handle_code(key, false);
             }
         }
 
@@ -216,23 +310,58 @@ function KeyboardAdapter(bus)
     }
 
     /**
-     * @param {number} chr
      * @param {boolean} keydown
      */
-    function handler(chr, keydown)
+    function handler(e, keydown)
     {
         if(!keyboard.bus)
         {
             return;
         }
 
-        if(chr >= charmap.length || charmap[chr] === 0)
+        if(!may_handle(e))
         {
-            console.log("Missing char in map: " + chr.toString(16));
-            return true;
+            return;
         }
 
-        var code = charmap[chr];
+        var code = translate(e);
+
+        if(!code)
+        {
+            console.log("Missing char in map: " + e.keyCode.toString(16));
+            return;
+        }
+
+        handle_code(code, keydown);
+
+        e.preventDefault();
+
+        return false;
+    }
+
+    /**
+     * @param {number} code
+     * @param {boolean} keydown
+     */
+    function handle_code(code, keydown)
+    {
+        if(keydown)
+        {
+            if(keys_pressed[code])
+            {
+                handle_code(code, false);
+            }
+        }
+        else
+        {
+            if(!keys_pressed[code])
+            {
+                // stray keyup
+                return;
+            }
+        }
+
+        keys_pressed[code] = keydown;
 
         if(!keydown)
         {
@@ -250,8 +379,6 @@ function KeyboardAdapter(bus)
         {
             send_to_controller(code);
         }
-
-        return false;
     }
 
     function send_to_controller(code)
