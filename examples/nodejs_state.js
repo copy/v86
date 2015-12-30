e8d8d6166b14723bfe8a1c1e895071d58b292f31
copy <copy@copy.sh>
2015-12-30 18:18:23 +0100

    Split nodejs example

--- examples/nodejs_state.js
+++ examples/nodejs_state.js
@@ -9,15 +9,14 @@ function readfile(path)
     return new Uint8Array(fs.readFileSync(path)).buffer;
 }
 
+console.log("Use F2 to save the state and F3 to restore.");
+
 var bios = readfile(__dirname + "/../bios/seabios.bin");
 var linux = readfile(__dirname + "/../images/linux.iso");
 
 process.stdin.setRawMode(true);
 process.stdin.resume();
 process.stdin.setEncoding("utf8");
-
-var boot_start = Date.now();
-var booted = false;
 
 console.log("Now booting, please stand by ...");
 
@@ -29,15 +28,10 @@ var emulator = new V86Starter({
 
 emulator.add_listener("serial0-output-char", function(chr)
 {
-    if(!booted)
-    {
-        var now = Date.now();
-        console.log("Took %dms to boot", now - boot_start);
-        booted = true;
-    }
-
     process.stdout.write(chr);
 });
+
+var state;
 
 process.stdin.on("data", function(c)
 {
@@ -45,6 +39,29 @@ process.stdin.on("data", function(c)
     {
         // ctrl c
         process.exit();
+    }
+    else if(c === "\x1b\x4f\x51")
+    {
+        // f2
+        emulator.save_state(function(err, s)
+        {
+            console.log("--- Saved ---");
+            if(err)
+            {
+                throw err;
+            }
+
+            state = s;
+        });
+    }
+    else if(c === "\x1b\x4f\x52")
+    {
+        // f3
+        if(state)
+        {
+            console.log("--- Restored ---");
+            emulator.restore_state(state);
+        }
     }
     else
     {
