c72d8de8cb90506774f4c340dff9cee9966c609a
copy <copy@copy.sh>
2017-03-15 20:12:59 -0500

    Add missing config.js file

--- Makefile
+++ Makefile
@@ -75,7 +75,7 @@ TRANSPILE_ES6_FLAGS=\
 
 CORE_FILES=src/const.js src/io.js src/main.js src/lib.js src/fpu.js src/ide.js src/pci.js src/floppy.js src/memory.js\
 	   src/dma.js src/pit.js src/vga.js src/ps2.js src/pic.js src/rtc.js src/uart.js src/hpet.js src/acpi.js src/apic.js\
-	   src/state.js src/ne2k.js src/virtio.js src/bus.js src/log.js\
+	   src/state.js src/ne2k.js src/virtio.js src/bus.js src/log.js src/config.js\
 	   src/cpu.js src/translate.js src/modrm.js src/string.js src/arith.js src/misc_instr.js src/instructions.js src/debug.js
 LIB_FILES=lib/9p.js lib/filesystem.js lib/jor1k.js lib/marshall.js lib/utf8.js
 BROWSER_FILES=src/browser/screen.js\
--- loader.js
+++ loader.js
@@ -3,10 +3,11 @@
 (function()
 {
     var CORE_FILES =
-        "const.js log.js cpu.js debug.js translate.js modrm.js string.js arith.js misc_instr.js instructions.js " +
+        "const.js config.js log.js cpu.js debug.js translate.js modrm.js string.js arith.js misc_instr.js instructions.js " +
         "io.js main.js lib.js ide.js fpu.js pci.js floppy.js " +
         "memory.js dma.js pit.js vga.js ps2.js pic.js rtc.js uart.js acpi.js apic.js hpet.js " +
         "ne2k.js state.js virtio.js bus.js";
+
     var BROWSER_FILES = "main.js screen.js keyboard.js mouse.js serial.js lib.js network.js starter.js worker_bus.js";
     //var LIB_FILES = "esprima.js walk.js";
     var LIB_FILES = "";
