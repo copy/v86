522dd0ada1d978baa552e1b7c434e6c1a58ff529
Amaan Cheval <amaan.cheval@gmail.com>
2017-05-08 16:51:30 +0530

    Add movd.asm test

---
+++ tests/nasm/movd.asm
@@ -0,0 +1,39 @@
+global _start
+
+section .data
+	align 16
+myquad:
+	dq	0x1234567890abcdef
+mydword:
+	dd	0xcafebabe
+myaddress:
+	dd	0xdeadbeef
+
+MBALIGN     equ  1<<0                   ; align loaded modules on page boundaries
+MEMINFO     equ  1<<1                   ; provide memory map
+FLAGS       equ  0                      ; this is the Multiboot 'flag' field
+MAGIC       equ  0x1BADB002             ; 'magic number' lets bootloader find the header
+CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum of above, to prove we are multiboot
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+section .text
+
+_start:
+main:
+
+	movd	mm0, [mydword]
+	movd	[myaddress], mm0
+	movd	mm1, [myaddress]
+	movd	eax, mm0
+	movd	mm4, eax
+
+loop:
+	hlt
+	jmp     loop
+
+;;; reg_mmxs should be:
+;;; "{"0":-889275714,"1":0,"2":-889275714,"3":0,"4":0,"5":0,"6":0,"7":0,"8":-889275714,"9":0,"10":0,"11":0,"12":0,"13":0,"14":0,"15":0}"
