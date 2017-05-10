fa7112dc4d0204afac5f8e4154422f612bb78aff
Amaan Cheval <amaan.cheval@gmail.com>
2017-05-10 11:51:04 +0530

    Implement psrlw mm, mm/m64

--- src/instructions.js
+++ src/instructions.js
@@ -3080,7 +3080,33 @@ t[0xCE] = cpu => { cpu.bswap(reg_esi); };
 t[0xCF] = cpu => { cpu.bswap(reg_edi); };
 
 t[0xD0] = cpu => { cpu.unimplemented_sse(); };
-t[0xD1] = cpu => { cpu.unimplemented_sse(); };
+t[0xD1] = cpu => {
+    // psrlw mm, mm/m64
+    dbg_assert((cpu.prefixes & (PREFIX_MASK_REP | PREFIX_MASK_OPSIZE)) == 0);
+
+    cpu.read_modrm_byte();
+    let source = cpu.read_xmm_mem64s();
+    let destination_low = cpu.reg_mmxs[2 * (cpu.modrm_byte >> 3 & 7)];
+    let destination_high = cpu.reg_mmxs[2 * (cpu.modrm_byte >> 3 & 7) + 1];
+
+    // JS will right shift as expected only if shift is < 32
+    let shift = source[0];
+    if ((shift >>> 0) > 31) {
+        shift = 31;
+    }
+
+    let word0 = (destination_low & 0xFFFF) >>> shift;
+    let word1 = (destination_low >>> 16) >>> shift;
+    let low = word0 | word1 << 16;
+
+    let word2 = (destination_high & 0xFFFF) >>> shift;
+    let word3 = (destination_high >>> 16) >>> shift;
+    let high = word2 | word3 << 16;
+
+    let data = cpu.create_atom64s(low, high);
+
+    cpu.write_xmm64s(data);
+};
 t[0xD2] = cpu => { cpu.unimplemented_sse(); };
 t[0xD3] = cpu => { cpu.unimplemented_sse(); };
 t[0xD4] = cpu => { cpu.unimplemented_sse(); };
