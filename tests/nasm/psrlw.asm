a0197c8722d8999748fff6cfe334af152f913bbb
Amaan Cheval <amaan.cheval@gmail.com>
2017-05-10 11:53:23 +0530

    Add psrlw mm, mm/m64 test (ignoring psrlw mm, imm8 for now)

---
+++ tests/nasm/psrlw.asm
@@ -0,0 +1,37 @@
+global _start
+
+section .data
+	align 16
+myquad:
+	dq	0xad0000ceadad00ff
+mydword:
+	dd	0xcafebac0
+myaddress:
+	dq	0x07
+
+MBALIGN     equ  1<<0                   ; align loaded modules on page boundaries
+MEMINFO     equ  1<<1                   ; provide memory map
+FLAGS       equ  0                      ; this is the Multiboot 'flag' field
+MAGIC       equ  0x1BADB002             ; 'magic number' lets bootloader find the header
+CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum of above, to prove we are multiboot
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+section .text
+
+_start:
+main:
+	movq		mm0, [myquad]
+	movq		mm1, [myquad]
+	psrlw		mm0, [myaddress]
+	psrlw		mm1, [mydword]
+loop:
+	hlt
+	jmp     loop
+
+;;; mm0 = {22740993, 22675457}
+;;; mm1 = {0, 0}
+;;; {"0":22740993,"1":22675457,"2":0,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0,"10":0,"11":0,"12":0,"13":0,"14":0,"15":0}
