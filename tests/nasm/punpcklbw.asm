67dd9decde085023527f554e72591359ac519b11
Amaan Cheval <amaan.cheval@gmail.com>
2017-05-10 12:51:01 +0530

    Add nasm test for punpcklbw mm, mm/m32

---
+++ tests/nasm/punpcklbw.asm
@@ -0,0 +1,35 @@
+global _start
+
+section .data
+	align 16
+myquad:
+	dq	0x00ad00adad007fff
+mydword:
+	dd	0xcafebabe
+myaddress:
+	dq	0x00adbeefc0de00ce
+
+MBALIGN     equ  1<<0                   ; align loaded modules on page boundaries
+MEMINFO     equ  1<<1                   ; provide memory map
+FLAGS       equ  0                      ; this is the Multiboot 'flag' field
+MAGIC       equ  0x1BADB002             ; 'magic number' lets bootloader find the header
+CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum of above, to prove we are multiboot
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+section .text
+
+_start:
+main:
+	movq		mm0, [myquad]
+	punpcklbw	mm0, [myaddress]
+
+loop:
+	hlt
+	jmp     loop
+
+;;; mm0 = {8376063, -1062347264}
+;;; {"0":8376063,"1":-1062347264,"2":0,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0,"10":0,"11":0,"12":0,"13":0,"14":0,"15":0}
