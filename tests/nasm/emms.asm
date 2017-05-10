9644b42d26218b2a0277f84f4b1c4f0aecf1f44f
Amaan Cheval <amaan.cheval@gmail.com>
2017-05-10 14:51:59 +0530

    Add nasm test for emms

---
+++ tests/nasm/emms.asm
@@ -0,0 +1,35 @@
+global _start
+
+section .data
+	align 16
+myquad:
+	dq	0xad0000ceadad00ff
+mydword:
+	dd	0xcafebabe
+myaddress:
+	dq	0x00adbeefc0de00ce
+
+MBALIGN     equ  1<<0                   ; align loaded modules on page boundaries
+MEMINFO     equ  1<<1                   ; provide memory map
+FLAGS       equ  0                      ; this is the Multiboot 'flag' field
+MAGIC       equ  0x1BADB002             ; 'magic number' lets bootloader find the header
+CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum of above, to prove we are multiboot
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+section .text
+
+_start:
+main:
+	fld	dword [mydword]
+	emms
+
+loop:
+	hlt
+	jmp     loop
+
+;;; fptag = 0xFFFF
+;;; cpu.fpu.load_tag_word() = 0xFFFF
