98286efc9b84312b5401f131b3119b5cf76364b7
Amaan Cheval <amaan.cheval@gmail.com>
2017-05-10 12:12:14 +0530

    Add test for psubw mm, mm/m64

---
+++ tests/nasm/psubw.asm
@@ -0,0 +1,35 @@
+global _start
+
+section .data
+	align 16
+myquad:
+	dq	0xad0000ceadad00ff
+mydword:
+	dd	0xcafebabe
+myaddress:
+	dq	0x00adbeefc0de00ce
+
+MBALIGN     equ  1<<0                   ; align loaded modules on page boundaries
+MEMINFO     equ  1<<1                   ; provide memory map
+FLAGS       equ  0                      ; this is the Multiboot 'flag' field
+MAGIC       equ  0x1BADB002             ; 'magic number' lets bootloader find the header
+CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum of above, to prove we are multiboot
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+section .text
+
+_start:
+main:
+	movq		mm0, [myquad]
+	psubw		mm0, [myaddress]
+
+loop:
+	hlt
+	jmp     loop
+
+;;; mm0 = {-321978319, -1403829793}
+;;; {"0":-321978319,"1":-1403829793,"2":0,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0,"10":0,"11":0,"12":0,"13":0,"14":0,"15":0}
