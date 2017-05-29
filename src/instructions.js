733e32187ee96c9ca9356d0e43322ea507a78fa1
Amaan Cheval <amaan.cheval@gmail.com>
2017-05-29 16:21:45 +0530

    Implement packsswb mm, mm/m64

--- src/instructions.js
+++ src/instructions.js
@@ -2453,7 +2453,32 @@ t[0x62] = cpu => {
     cpu.write_xmm64s(data);
 };
 
-t[0x63] = cpu => { cpu.unimplemented_sse(); };
+t[0x63] = cpu => {
+    // packsswb mm, mm/m64
+    dbg_assert((cpu.prefixes & (PREFIX_MASK_REP | PREFIX_MASK_OPSIZE)) == 0);
+
+    cpu.read_modrm_byte();
+    let source = cpu.read_xmm_mem64s();
+    let destination_low = cpu.reg_mmxs[2 * (cpu.modrm_byte >> 3 & 7)];
+    let destination_high = cpu.reg_mmxs[2 * (cpu.modrm_byte >> 3 & 7) + 1];
+
+    let low = 0;
+    low |= (cpu.saturate_sw_to_sb((destination_low) & 0xFFFF));
+    low |= (cpu.saturate_sw_to_sb(destination_low >>> 16)) << 8;
+    low |= (cpu.saturate_sw_to_sb((destination_high) & 0xFFFF)) << 16;
+    low |= (cpu.saturate_sw_to_sb(destination_high >>> 16)) << 24;
+
+    let high = 0;
+    high |= (cpu.saturate_sw_to_sb((source[0]) & 0xFFFF));
+    high |= (cpu.saturate_sw_to_sb(source[0] >>> 16)) << 8;
+    high |= (cpu.saturate_sw_to_sb((source[1]) & 0xFFFF)) << 16;
+    high |= (cpu.saturate_sw_to_sb(source[1] >>> 16)) << 24;
+
+    let data = cpu.create_atom64s(low, high);
+
+    cpu.write_xmm64s(data);
+};
+
 t[0x64] = cpu => { cpu.unimplemented_sse(); };
 t[0x65] = cpu => { cpu.unimplemented_sse(); };
 t[0x66] = cpu => { cpu.unimplemented_sse(); };
