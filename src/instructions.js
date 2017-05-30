f51aed182f7b7141aad04b9752e0e1d6a7c62c4c
Amaan Cheval <amaan.cheval@gmail.com>
2017-05-30 11:35:36 +0530

    Implement pcmpgtd mm, mm/m64

--- src/instructions.js
+++ src/instructions.js
@@ -2563,7 +2563,30 @@ t[0x65] = cpu => {
     cpu.write_xmm64s(data);
 };
 
-t[0x66] = cpu => { cpu.unimplemented_sse(); };
+t[0x66] = cpu => {
+    // pcmpgtd mm, mm/m64
+    dbg_assert((cpu.prefixes & (PREFIX_MASK_REP | PREFIX_MASK_OPSIZE)) == 0);
+
+    cpu.read_modrm_byte();
+    let source = cpu.read_xmm_mem64s();
+    let destination_low = cpu.reg_mmxs[2 * (cpu.modrm_byte >> 3 & 7)];
+    let destination_high = cpu.reg_mmxs[2 * (cpu.modrm_byte >> 3 & 7) + 1];
+
+    let low = 0;
+    let high = 0;
+
+    if (destination_low > source[0]) {
+        low = 0xFFFFFFFF;
+    }
+
+    if (destination_high > source[1]) {
+        high = 0xFFFFFFFF;
+    }
+
+    let data = cpu.create_atom64s(low, high);
+
+    cpu.write_xmm64s(data);
+};
 
 t[0x67] = cpu => {
     // packuswb mm, mm/m64
