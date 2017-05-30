6cd503ebdae9503eb3ee096a69205ee88a3ef95c
Amaan Cheval <amaan.cheval@gmail.com>
2017-05-30 11:06:14 +0530

    Implement pcmpgtw mm, mm/m64

--- src/instructions.js
+++ src/instructions.js
@@ -2527,7 +2527,42 @@ t[0x64] = cpu => {
     cpu.write_xmm64s(data);
 };
 
-t[0x65] = cpu => { cpu.unimplemented_sse(); };
+t[0x65] = cpu => {
+    // pcmpgtw mm, mm/m64
+    dbg_assert((cpu.prefixes & (PREFIX_MASK_REP | PREFIX_MASK_OPSIZE)) == 0);
+
+    cpu.read_modrm_byte();
+    let source = cpu.read_xmm_mem64s();
+    let destination_low = cpu.reg_mmxs[2 * (cpu.modrm_byte >> 3 & 7)];
+    let destination_high = cpu.reg_mmxs[2 * (cpu.modrm_byte >> 3 & 7) + 1];
+
+    let word0 = 0;
+    let word1 = 0;
+    let word2 = 0;
+    let word3 = 0;
+
+    if ((destination_low << 16 >> 16) > (source[0] << 16 >> 16)) {
+        word0 = 0xFFFF;
+    }
+    if ((destination_low >> 16) > (source[0] >> 16)) {
+        word1 = 0xFFFF;
+    }
+
+    if ((destination_high << 16 >> 16) > (source[1] << 16 >> 16)) {
+        word2 = 0xFFFF;
+    }
+    if ((destination_high >> 16) > (source[1] >> 16)) {
+        word3 = 0xFFFF;
+    }
+
+    let low = word0 | word1 << 16;
+    let high = word2 | word3 << 16;
+
+    let data = cpu.create_atom64s(low, high);
+
+    cpu.write_xmm64s(data);
+};
+
 t[0x66] = cpu => { cpu.unimplemented_sse(); };
 
 t[0x67] = cpu => {
