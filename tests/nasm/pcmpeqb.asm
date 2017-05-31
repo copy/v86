fb07954f1034ecdc85df0db82c33f2cc46925cd3
Amaan Cheval <amaan.cheval@gmail.com>
2017-05-31 20:23:34 +0530

    Add nasm test for pcmpeqb

---
+++ tests/nasm/pcmpeqb.asm
@@ -0,0 +1,64 @@
+global _start
+
+section .data
+	align 16
+quad1:
+	dq	0x00ad00adad007fff
+quad2:
+	dq	0x7fff00428000ffff
+quad3:
+	dq	0x01008080f0f0ff42
+mydword:
+	dd	0xcafebabe
+myaddress:
+	dq	0x00adbeefc0de00ce
+
+MBALIGN     equ  1<<0                   ; align loaded modules on page boundaries
+MEMINFO     equ  1<<1                   ; provide memory map
+FLAGS       equ  0                      ; this is the Multiboot 'flag' field
+MAGIC       equ  0x1BADB002             ; 'magic number' lets bootloader find the header
+CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum of above, to prove we are multiboot
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+section .text
+
+_start:
+main:
+	movq		mm0, [quad1]
+	movq		mm1, [quad2]
+	movq		mm2, [quad3]
+	movq		mm3, [quad2]
+
+	pcmpeqb	mm0, [quad2]
+	pcmpeqb	mm0, [quad1]
+	pcmpeqb	mm1, [quad3]
+	pcmpeqb	mm2, [quad1]
+	pcmpeqb	mm3, [quad1]
+
+
+loop:
+	hlt
+	jmp     loop
+
+;; {
+;;     "0": 255,
+;;     "1": -16777216,
+;;     "2": 65280,
+;;     "3": 0,
+;;     "4": 0,
+;;     "5": 0,
+;;     "6": 16711935,
+;;     "7": 65280,
+;;     "8": 0,
+;;     "9": 0,
+;;     "10": 0,
+;;     "11": 0,
+;;     "12": 0,
+;;     "13": 0,
+;;     "14": 0,
+;;     "15": 0
+;; }
