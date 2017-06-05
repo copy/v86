7aada50848042eaf0de4a5c1f2c1614bfc15d7a2
Amaan Cheval <amaan.cheval@gmail.com>
2017-06-05 17:09:52 +0530

    Implement por mm, mm/m64

--- src/instructions.js
+++ src/instructions.js
@@ -4134,7 +4134,24 @@ t[0xE9] = cpu => {
 };
 
 t[0xEA] = cpu => { cpu.unimplemented_sse(); };
-t[0xEB] = cpu => { cpu.unimplemented_sse(); };
+
+t[0xEB] = cpu => {
+    // por mm, mm/m64
+    dbg_assert((cpu.prefixes & (PREFIX_MASK_REP | PREFIX_MASK_OPSIZE)) == 0);
+
+    cpu.read_modrm_byte();
+    let source = cpu.read_xmm_mem64s();
+    let destination_low = cpu.reg_mmxs[2 * (cpu.modrm_byte >> 3 & 7)];
+    let destination_high = cpu.reg_mmxs[2 * (cpu.modrm_byte >> 3 & 7) + 1];
+
+    let data = cpu.create_atom64s(
+        (source[0] | destination_low),
+        (source[1] | destination_high)
+    );
+
+    cpu.write_xmm64s(data);
+};
+
 t[0xEC] = cpu => { cpu.unimplemented_sse(); };
 t[0xED] = cpu => { cpu.unimplemented_sse(); };
 t[0xEE] = cpu => { cpu.unimplemented_sse(); };
