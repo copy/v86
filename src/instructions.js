cf2b40d5be16a42a4419e771cfc35c807c32cf79
Amaan Cheval <amaan.cheval@gmail.com>
2017-06-05 17:32:41 +0530

    Implement paddsw mm, mm/m64

--- src/instructions.js
+++ src/instructions.js
@@ -4174,7 +4174,36 @@ t[0xEC] = cpu => {
     cpu.write_xmm64s(data);
 };
 
-t[0xED] = cpu => { cpu.unimplemented_sse(); };
+t[0xED] = cpu => {
+    // paddsw mm, mm/m64
+    dbg_assert((cpu.prefixes & (PREFIX_MASK_REP | PREFIX_MASK_OPSIZE)) == 0);
+
+    cpu.read_modrm_byte();
+    let source = cpu.read_xmm_mem64s();
+    let destination_low = cpu.reg_mmxs[2 * (cpu.modrm_byte >> 3 & 7)];
+    let destination_high = cpu.reg_mmxs[2 * (cpu.modrm_byte >> 3 & 7) + 1];
+
+    let word0 = cpu.saturate_sd_to_sw(
+        (destination_low << 16 >> 16) + (source[0] << 16 >> 16)
+    );
+    let word1 = cpu.saturate_sd_to_sw(
+        (destination_low >> 16) + (source[0] >> 16)
+    );
+    let word2 = cpu.saturate_sd_to_sw(
+        (destination_high << 16 >> 16) + (source[1] << 16 >> 16)
+    );
+    let word3 = cpu.saturate_sd_to_sw(
+        (destination_high >> 16) + (source[1] >> 16)
+    );
+
+    let low = word0 | word1 << 16;
+    let high = word2 | word3 << 16;
+
+    let data = cpu.create_atom64s(low, high);
+
+    cpu.write_xmm64s(data);
+};
+
 t[0xEE] = cpu => { cpu.unimplemented_sse(); };
 t[0xEF] = cpu => {
     // pxor mm, mm/m64
