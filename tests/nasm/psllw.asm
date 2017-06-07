e5176f4e538d9d5a443b1ebe8a844ead3e3a35f7
Amaan Cheval <amaan.cheval@gmail.com>
2017-06-07 15:44:20 +0530

    Add regression test for psllw bug

--- tests/nasm/psllw.asm
+++ tests/nasm/psllw.asm
@@ -2,14 +2,18 @@ global _start
 
 section .data
 	align 16
-myquad:
+quad1:
 	dq	0xad0000ceadad00ff
+quad2:
+	dq	0x42ff88ff11aabbcc
 mydword:
 	dd	0xcafebac0
 shift1:
 	dq	0x07
 shift2:
 	dq	-0x22
+shift3:
+	dq	8
 
 MBALIGN     equ  1<<0                   ; align loaded modules on page boundaries
 MEMINFO     equ  1<<1                   ; provide memory map
@@ -26,26 +30,21 @@ section .text
 
 _start:
 main:
-	movq		mm0, [myquad]
-	movq		mm1, [myquad]
-	movq		mm2, [myquad]
-	movq		mm6, [myquad]
+	movq		mm0, [quad1]
+	movq		mm1, [quad1]
+	movq		mm2, [quad1]
+	movq		mm3, [quad2]
+	movq		mm4, [quad2]
+	movq		mm6, [quad2]
 
 	psllw		mm0, [shift1]
 	psllw		mm1, 0xff
 	psllw		mm2, [shift2]
+	psllw		mm3, 12
+	psllw		mm4, [shift3]
 	psllw		mm6, 0x5
 
 loop:
 	hlt
 	jmp     loop
 
-;;; mm0 = {-696221824, -2147457280}
-;;; mm1 = {0, 0}
-;;; mm2 = {0, 0}
-;;; mm3 = {0, 0}
-;;; mm4 = {0, 0}
-;;; mm5 = {0, 0}
-;;; mm6 = {-1247797280, -1610606144}
-;;; mm7 = {0, 0}
-;;; {"0":-696221824,"1":-2147457280,"2":0,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0,"10":0,"11":0,"12":-1247797280,"13":-1610606144,"14":0,"15":0}
