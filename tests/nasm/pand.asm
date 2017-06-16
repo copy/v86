4d5fe0309cef43af6419f3f6fa19d270636d72a1
Amaan Cheval <amaan.cheval@gmail.com>
2017-06-16 09:47:52 +0530

    Improve pand tests

--- tests/nasm/pand.asm
+++ tests/nasm/pand.asm
@@ -2,12 +2,16 @@ global _start
 
 section .data
 	align 16
-myquad:
+quad1:
 	dq	0xad0000ceadad00ff
+quad2:
+	dq	0xffffffffffffffff
+quad3:
+	dq	0x0000000000000000
+quad4:
+	dq	0x7fff8000ffff0808
 mydword:
 	dd	0xcafebabe
-myaddress:
-	dq	0x00adbeefc0de00ce
 
 MBALIGN     equ  1<<0                   ; align loaded modules on page boundaries
 MEMINFO     equ  1<<1                   ; provide memory map
@@ -24,8 +28,22 @@ section .text
 
 _start:
 main:
-	movq		mm0, [myquad]
-	pand		mm0, [myaddress]
+	movq		mm0, [quad1]
+	movq		mm1, [quad2]
+	movq		mm2, [quad3]
+	movq		mm3, [quad4]
+	movq		mm4, [quad1]
+
+	pand		mm1, [quad2]
+	pand		mm1, mm2
+	pand		mm2, [quad1]
+	pand		mm2, mm3
+	pand		mm3, [quad3]
+	pand		mm3, mm4
+	pand		mm4, [quad1]
+	pand		mm4, mm1
+	pand		mm4, mm3
+
 
 loop:
 	hlt
