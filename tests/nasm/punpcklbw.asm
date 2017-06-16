58812d3d622caa900684fc1a27706e7eceb77c1b
Amaan Cheval <amaan.cheval@gmail.com>
2017-06-16 09:56:50 +0530

    Improve punpcklbw tests

--- tests/nasm/punpcklbw.asm
+++ tests/nasm/punpcklbw.asm
@@ -2,6 +2,14 @@ global _start
 
 section .data
 	align 16
+quad1:
+	dq	0xad0000ceadad00ff
+quad2:
+	dq	0xffffffffffffffff
+quad3:
+	dq	0x0000000000000000
+quad4:
+	dq	0x7fff8000ffff0808
 myquad:
 	dq	0x00ad00adad007fff
 mydword:
@@ -24,8 +32,27 @@ section .text
 
 _start:
 main:
-	movq		mm0, [myquad]
-	punpcklbw	mm0, [myaddress]
+	movq		mm0, [quad1]
+	movq		mm1, [quad2]
+	movq		mm2, [quad3]
+	movq		mm3, [quad4]
+	movq		mm4, [quad1]
+	movq		mm5, [quad2]
+	movq		mm6, [quad3]
+	movq		mm7, [quad4]
+
+	punpcklbw	mm4, mm0
+	punpcklbw	mm5, mm1
+	punpcklbw	mm6, mm2
+	punpcklbw	mm7, mm3
+	punpcklbw	mm0, [quad2]
+	punpcklbw	mm1, [quad3]
+	punpcklbw	mm2, [quad4]
+	punpcklbw	mm3, [quad1]
+	punpcklbw	mm0, mm5
+	punpcklbw	mm1, mm6
+	punpcklbw	mm2, mm7
+	punpcklbw	mm3, mm4
 
 loop:
 	hlt
