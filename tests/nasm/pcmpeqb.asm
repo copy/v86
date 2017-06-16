b6a81a3f71abdfc0616d898dba3ebf8f37210c71
Amaan Cheval <amaan.cheval@gmail.com>
2017-06-16 09:56:58 +0530

    Improve pcmpeqb tests

--- tests/nasm/pcmpeqb.asm
+++ tests/nasm/pcmpeqb.asm
@@ -8,6 +8,8 @@ quad2:
 	dq	0x7fff00428000ffff
 quad3:
 	dq	0x01008080f0f0ff42
+quad4:
+	dq	0x0000000000000000
 mydword:
 	dd	0xcafebabe
 myaddress:
@@ -31,13 +33,18 @@ main:
 	movq		mm0, [quad1]
 	movq		mm1, [quad2]
 	movq		mm2, [quad3]
-	movq		mm3, [quad2]
+	movq		mm3, [quad4]
+	movq		mm4, [quad1]
 
-	pcmpeqb	mm0, [quad2]
-	pcmpeqb	mm0, [quad1]
-	pcmpeqb	mm1, [quad3]
-	pcmpeqb	mm2, [quad1]
-	pcmpeqb	mm3, [quad1]
+	pand		mm1, [quad2]
+	pand		mm1, mm2
+	pand		mm2, [quad1]
+	pand		mm2, mm3
+	pand		mm3, [quad3]
+	pand		mm3, mm4
+	pand		mm4, [quad1]
+	pand		mm4, mm1
+	pand		mm4, mm3
 
 
 loop:
