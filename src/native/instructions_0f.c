e03ada1e86d894c6fc100cca2ae145d16e42ad86
Amaan Cheval <amaan.cheval@gmail.com>
2017-12-06 13:39:43 +0530

    Implement psraw xmm, imm8 (sse2)

--- src/native/instructions_0f.c
+++ src/native/instructions_0f.c
@@ -1775,7 +1775,6 @@ static void instr_660F71_2_mem(int32_t addr, int32_t r) { trigger_ud(); }
 static void instr_660F71_4_mem(int32_t addr, int32_t r) { trigger_ud(); }
 static void instr_660F71_6_mem(int32_t addr, int32_t r) { trigger_ud(); }
 
-static void instr_660F71_4_reg(int32_t r1, int32_t r2) { unimplemented_sse(); }
 static void instr_660F71_6_reg(int32_t r1, int32_t r2) { unimplemented_sse(); }
 
 static void instr_660F71_2_reg(int32_t r, int32_t imm8) {
@@ -1805,6 +1804,32 @@ static void instr_660F71_2_reg(int32_t r, int32_t imm8) {
         int32_t word7 = ((uint32_t) destination.u16[7]) >> imm8;
         dword3 = word6 | word7 << 16;
     }
+
+    write_xmm128(r, dword0, dword1, dword2, dword3);
+}
+
+static void instr_660F71_4_reg(int32_t r, int32_t imm8) {
+    // psraw xmm, imm8
+    task_switch_test_mmx();
+    union reg128 destination = read_xmm128s(r);
+
+    int32_t shift = imm8 > 15 ? 16 : imm8;
+
+    int32_t word0 = (destination.i16[0] >> shift) & 0xFFFF;
+    int32_t word1 = (destination.i16[1] >> shift) & 0xFFFF;
+    int32_t dword0 = word0 | word1 << 16;
+
+    int32_t word2 = (destination.i16[2] >> shift) & 0xFFFF;
+    int32_t word3 = (destination.i16[3] >> shift) & 0xFFFF;
+    int32_t dword1 = word2 | word3 << 16;
+
+    int32_t word4 = (destination.i16[4] >> shift) & 0xFFFF;
+    int32_t word5 = (destination.i16[5] >> shift) & 0xFFFF;
+    int32_t dword2 = word4 | word5 << 16;
+
+    int32_t word6 = (destination.i16[6] >> shift) & 0xFFFF;
+    int32_t word7 = (destination.i16[7] >> shift) & 0xFFFF;
+    int32_t dword3 = word6 | word7 << 16;
 
     write_xmm128(r, dword0, dword1, dword2, dword3);
 }
