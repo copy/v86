81fe37f21dab87085c225ccd6f6bb18e2907a8e4
Amaan Cheval <amaan.cheval@gmail.com>
2017-12-06 21:53:50 +0530

    Implement pslld xmm, imm8 (sse2)

--- gen/x86_table.js
+++ gen/x86_table.js
@@ -493,7 +493,7 @@ const encodings = [
     { opcode: 0x0F72, e: 1, fixed_g: 4, imm8: 1, only_reg: 1, },
     { opcode: 0x660F72, e: 1, fixed_g: 4, imm8: 1, only_reg: 1 },
     { opcode: 0x0F72, e: 1, fixed_g: 6, imm8: 1, only_reg: 1, },
-    { opcode: 0x660F72, e: 1, fixed_g: 6, imm8: 1, only_reg: 1, skip: 1, },
+    { opcode: 0x660F72, e: 1, fixed_g: 6, imm8: 1, only_reg: 1 },
 
     { opcode: 0x0F73, e: 1, fixed_g: 2, imm8: 1, only_reg: 1, },
     { opcode: 0x660F73, e: 1, fixed_g: 2, imm8: 1, only_reg: 1, },
