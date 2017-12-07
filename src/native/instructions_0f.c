f80903ae77e60e7f2358171dec8f55677ab8a225
Amaan Cheval <amaan.cheval@gmail.com>
2017-12-07 17:39:51 +0530

    Implement psrlw xmm, xmm/m128 (sse2)

--- src/native/instructions_0f.c
+++ src/native/instructions_0f.c
@@ -2780,8 +2780,11 @@ static void instr_0FD1(union reg64 source, int32_t r) {
 }
 DEFINE_SSE_SPLIT(instr_0FD1, safe_read64s, read_mmx64s)
 
-static void instr_660FD1_mem(int32_t addr, int32_t r) { unimplemented_sse(); }
-static void instr_660FD1_reg(int32_t r1, int32_t r2) { unimplemented_sse(); }
+static void instr_660FD1(union reg128 source, int32_t r) {
+    // psrlw xmm, xmm/m128
+    psrlw_r128(r, source.u32[0]);
+}
+DEFINE_SSE_SPLIT(instr_660FD1, safe_read128s, read_xmm128s)
 
 static void instr_0FD2(union reg64 source, int32_t r) {
     // psrld mm, mm/m64
