52b43b00bc90507b66e7eb23f38e622459180155
Amaan Cheval <amaan.cheval@gmail.com>
2017-12-12 17:29:58 +0530

    Implement pmaxsw mm, mm/m64

--- gen/x86_table.js
+++ gen/x86_table.js
@@ -589,7 +589,7 @@ const encodings = [
     { opcode: 0x660FEC, e: 1 },
     { opcode: 0x0FED, e: 1 },
     { opcode: 0x660FED, e: 1 },
-    { opcode: 0x0FEE, e: 1, skip: 1, },
+    { opcode: 0x0FEE, e: 1 },
     { opcode: 0x660FEE, e: 1, skip: 1, },
     { opcode: 0x0FEF, e: 1 },
     { opcode: 0x660FEF, e: 1 },
