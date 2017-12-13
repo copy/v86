d98f119c406b17d600f77f07dac7dffc334ae51e
Amaan Cheval <amaan.cheval@gmail.com>
2017-12-13 11:49:52 +0530

    Reorder sse_instr.h to follow natural opcode order

--- src/native/sse_instr.h
+++ src/native/sse_instr.h
@@ -8,13 +8,13 @@ void mov_r_m128(int32_t addr, int32_t r);
 void mov_rm_r128(union reg128 source, int32_t r);
 void movh_m64_r128(int32_t addr, int32_t r);
 void movh_r128_m64(int32_t addr, int32_t r);
+void psrlw_r128(int32_t r, uint32_t shift);
+void psraw_r128(int32_t r, uint32_t shift);
+void psllw_r128(int32_t r, uint32_t shift);
+void psrld_r128(int32_t r, uint32_t shift);
+void psrad_r128(int32_t r, uint32_t shift);
+void pslld_r128(int32_t r, uint32_t shift);
 void psrlq_r128(int32_t r, uint32_t shift);
 void psllq_r128(int32_t r, uint32_t shift);
-void pslld_r128(int32_t r, uint32_t shift);
-void psllw_r128(int32_t r, uint32_t shift);
-void psraw_r128(int32_t r, uint32_t shift);
-void psrlw_r128(int32_t r, uint32_t shift);
-void psrad_r128(int32_t r, uint32_t shift);
-void psrld_r128(int32_t r, uint32_t shift);
 
 #endif
