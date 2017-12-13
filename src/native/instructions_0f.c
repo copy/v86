0100369eaf88e30eb0375ee14286367942a73e6b
Amaan Cheval <amaan.cheval@gmail.com>
2017-12-13 10:08:51 +0530

    Implement psubq mm, mm/m64 (sse2)

--- src/native/instructions_0f.c
+++ src/native/instructions_0f.c
@@ -4001,8 +4001,16 @@ static void instr_660FFA(union reg128 source, int32_t r) {
 }
 DEFINE_SSE_SPLIT(instr_660FFA, safe_read128s, read_xmm128s)
 
-static void instr_0FFB_mem(int32_t addr, int32_t r) { unimplemented_sse(); }
-static void instr_0FFB_reg(int32_t r1, int32_t r2) { unimplemented_sse(); }
+static void instr_0FFB(union reg64 source, int32_t r) {
+    // psubq mm, mm/m64
+    task_switch_test_mmx();
+    union reg64 destination = read_mmx64s(r);
+
+    destination.u64[0] = (uint64_t) destination.u64[0] - (uint64_t) source.u64[0];
+    write_mmx_reg64(r, destination);
+}
+DEFINE_SSE_SPLIT(instr_0FFB, safe_read64s, read_mmx64s)
+
 static void instr_660FFB_mem(int32_t addr, int32_t r) { unimplemented_sse(); }
 static void instr_660FFB_reg(int32_t r1, int32_t r2) { unimplemented_sse(); }
 
