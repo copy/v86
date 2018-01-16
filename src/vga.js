cf462a3c398ea77c577649b41595316f391d9c18
Ernest Wong <ewon521@gmail.com>
2018-01-16 18:47:49 +1300

    DAC linked list: Tell screen adapter to update

--- src/vga.js
+++ src/vga.js
@@ -2422,6 +2422,9 @@ VGAScreen.prototype.vga_redraw = function()
                 buffer[addr] = color;
                 addr = dac_next[addr];
             }
+
+            // Tell screen adapter to redraw all via start and end pixel
+            this.complete_redraw();
         }
     }
     else
@@ -2458,6 +2461,9 @@ VGAScreen.prototype.vga_redraw = function()
                 buffer[addr] = color;
                 addr = dac_next[addr];
             }
+
+            // Tell screen adapter to redraw all via start and end pixel
+            this.complete_redraw();
         }
     }
 };
