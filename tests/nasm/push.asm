b1787e5a0fb6d20119ee91bc2f231717acbe09b8
Amaan Cheval <amaan.cheval@gmail.com>
2018-02-26 10:17:49 +0530

    Clean up push nasm test

--- tests/nasm/push.asm
+++ tests/nasm/push.asm
@@ -2,44 +2,43 @@ global _start
 
 section .data
     align 16
-myquad:
-    dq	0x1234567890abcdef
-mydword:
-    dd	0xcafebabe
 myaddress:
-    dd	0xdeadbeef
+    dd  0xdeadbeef
 
 %include "header.inc"
 
     ;; push r/m - push edx
-    db 0xff
-    db 0xf2
+    db      0xff
+    db      0xf2
 
     ;; push r/m - push bx
-    db 0x66
-    db 0xff
-    db 0xf3
+    db      0x66
+    db      0xff
+    db      0xf3
 
-    push	0xdeadbeef
-    push	WORD 0xd00d
-    push	DWORD [myaddress]
+    ;; push imm
+    push    0xdeadbeef
+    push    WORD 0xd00d
 
-    lea	eax, [myaddress]
-    push	WORD [eax]
+    ;; push r/m - mem
+    push    DWORD [myaddress]
+    lea     eax, [myaddress]
+    push    WORD [eax]
 
-    mov	ecx, 0xcafe
-    push	cx
-    push	ecx
+    ;; push reg
+    mov     ecx, 0xcafe
+    push    cx
+    push    ecx
 
-    xor	eax, eax
+    xor     eax, eax
 
-    pop	ax
-    pop	eax
-    pop	cx
-    pop	ecx
-    pop	dx
-    pop	ebx
-    pop	si
-    pop	di
+    pop     ax
+    pop     eax
+    pop     cx
+    pop     ecx
+    pop     dx
+    pop     ebx
+    pop     si
+    pop     di
 
 %include "footer.inc"
