5d6e6d2e8f0edf69301bddd76ae3e0f651d037f1
Fabian <copy@copy.sh>
2018-02-27 15:57:40 -0600

    Remove dbg_trace from non-debug builds

--- src/native/log.h
+++ src/native/log.h
@@ -9,5 +9,6 @@ extern void logop(int32_t, int32_t);
 extern void dbg_trace(void);
 
 #define dbg_log(...) { if(DEBUG) { printf(__VA_ARGS__); } }
+#define dbg_trace(...) { if(DEBUG) { dbg_trace(__VA_ARGS__); } }
 #define dbg_assert(condition) { if(DEBUG) { if(!(condition)) dbg_log(#condition); assert(condition); } }
 #define dbg_assert_message(condition, message) { if(DEBUG && !(condition)) { dbg_log(message); assert(false); } }
