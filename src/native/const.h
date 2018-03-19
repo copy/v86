f1dc0330e9102923572e84b8ae778da9058aa887
Fabian <copy@copy.sh>
2018-03-19 18:08:20 -0500

    Disable tlb invariant checking by default

--- src/native/const.h
+++ src/native/const.h
@@ -189,7 +189,7 @@
 #define JIT_NEXT_BLOCK_BRANCHED "next_block_branched"
 #define JIT_NEXT_BLOCK_NOT_BRANCHED "next_block_not_branched"
 
-#define CHECK_TLB_INVARIANTS 1
+#define CHECK_TLB_INVARIANTS 0
 
 #define JIT_NEXT_BLOCK_BRANCHED_IDX 0
 #define JIT_NEXT_BLOCK_NOT_BRANCHED_IDX 1
