56e74baed063073f0ddbe3098209044f5ed43c2c
Amaan Cheval <amaan.cheval@gmail.com>
2018-04-21 03:15:58 +0530

    nasmtests/jit: Regression test for mov16 (confirm 16-bit, not 32-bit)
    This regression test will _NOT_ currently catch regressions in "make
    nasmtests-force-jit" due to a pending issue where JIT execution doesn't
    follow through the popf instruction, which this and most tests use
    through header.inc.
    
    To see the test actually fail, you need:
    - To remove the popf instruction
    - s/load_u16/load_i32/ in gen_set_reg16_r

---
+++ tests/nasm/mov16.asm
@@ -0,0 +1,48 @@
+;;; Test JIT optimization of opcodes 0x89 and 0x8b
+
+global _start
+
+section .data
+    align 16
+mydword:
+    dd  0xcafebabe
+myaddress:
+    dd  0xdeadbeef
+
+%include "header.inc"
+
+    ;; Load 32-bit values to confirm that the 16-bit movs do not overwrite existing values here
+    mov eax, 0xcafeb055
+    mov esi, 0x1bada551
+
+    mov ecx, [mydword]
+    mov edx, [myaddress]
+
+    ;; Skipping 16-bit 0x89 mem tests since they cause seg faults in gdb:
+    ;; lea di, [myaddress]
+    ;; lea si, [mydword]
+    ;; mov [di+bx], ax
+    ;; mov [si+bx], cx
+
+    ;; The following db's are used since mov reg, reg can be accomplished with several opcodes but
+    ;; we want to test these specific ones
+
+    ;; mov cx, si
+    db 0x66
+    db 0x89
+    db 0xf1
+    ;; mov dx, di
+    db 0x66
+    db 0x89
+    db 0xfa
+
+    ;; mov dx, ax
+    db 0x66
+    db 0x8b
+    db 0xd0
+    ;; mov ax, cx
+    db 0x66
+    db 0x8b
+    db 0xc1
+
+%include "footer.inc"
