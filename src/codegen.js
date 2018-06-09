fdc025f0d505d6b9b55ddfbf0c15a14a5ae5011b
Fabian <copy@copy.sh>
2018-06-09 13:33:49 -0500

    Increase buffers

--- src/codegen.js
+++ src/codegen.js
@@ -18,7 +18,7 @@ Codegen.prototype.reset = function()
 };
 
 Codegen.OUTPUT_OFFSET = 0x1000;
-Codegen.STR_INPUT_OFFSET = 0x31000;
+Codegen.STR_INPUT_OFFSET = 0x301000;
 
 Codegen.prototype.str_input = function(str)
 {
