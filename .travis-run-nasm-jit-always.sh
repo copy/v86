2f1ff12f9153063dc3320215773ac2a0d90cf347
Awal Garg <awalgarg@gmail.com>
2018-06-30 07:48:56 +0530

    remove travis scripts

--- .travis-run-nasm-jit-always.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-./tests/nasm/create_tests.js
-make -j $(nproc --all) nasmtests-force-jit
--- .travis-run-unit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-make build/libv86.js
-(cd tests/kvm-unit-tests && ./configure && make)
-tests/kvm-unit-tests/run.js tests/kvm-unit-tests/x86/realmode.flat
