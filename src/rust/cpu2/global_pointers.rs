a795accee61b7cb24cf896488b1902c70ff4e628
Fabian <copy@copy.sh>
2018-08-18 22:10:10 -0500

    c2rust cleanup: Make global pointers constants

--- src/rust/cpu2/global_pointers.rs
+++ src/rust/cpu2/global_pointers.rs
@@ -10,125 +10,64 @@
 
 use cpu2::cpu::{reg128, reg64};
 
-#[no_mangle]
-pub static mut reg8: *mut u8 = unsafe { 4i32 as *mut u8 };
-#[no_mangle]
-pub static mut reg16: *mut u16 = unsafe { 4i32 as *mut u16 };
-#[no_mangle]
-pub static mut reg8s: *mut i8 = unsafe { 4i32 as *mut i8 };
-#[no_mangle]
-pub static mut reg16s: *mut i16 = unsafe { 4i32 as *mut i16 };
-#[no_mangle]
-pub static mut reg32s: *mut i32 = unsafe { 4i32 as *mut i32 };
-#[no_mangle]
-pub static mut last_op1: *mut i32 = unsafe { 512i32 as *mut i32 };
-#[no_mangle]
-pub static mut last_op2: *mut i32 = unsafe { 516i32 as *mut i32 };
-#[no_mangle]
-pub static mut last_op_size: *mut i32 = unsafe { 520i32 as *mut i32 };
-#[no_mangle]
-pub static mut last_add_result: *mut i32 = unsafe { 524i32 as *mut i32 };
-#[no_mangle]
-pub static mut last_result: *mut i32 = unsafe { 528i32 as *mut i32 };
-#[no_mangle]
-pub static mut flags_changed: *mut i32 = unsafe { 532i32 as *mut i32 };
-#[no_mangle]
-pub static mut flags: *mut i32 = unsafe { 536i32 as *mut i32 };
-#[no_mangle]
-pub static mut page_fault: *mut bool = unsafe { 540i32 as *mut bool };
-#[no_mangle]
-pub static mut a20_enabled: *mut bool = unsafe { 552i32 as *mut bool };
-#[no_mangle]
-pub static mut instruction_pointer: *mut i32 = unsafe { 556i32 as *mut i32 };
-#[no_mangle]
-pub static mut previous_ip: *mut i32 = unsafe { 560i32 as *mut i32 };
-#[no_mangle]
-pub static mut idtr_size: *mut i32 = unsafe { 564i32 as *mut i32 };
-#[no_mangle]
-pub static mut idtr_offset: *mut i32 = unsafe { 568i32 as *mut i32 };
-#[no_mangle]
-pub static mut gdtr_size: *mut i32 = unsafe { 572i32 as *mut i32 };
-#[no_mangle]
-pub static mut gdtr_offset: *mut i32 = unsafe { 576i32 as *mut i32 };
-#[no_mangle]
-pub static mut cr: *mut i32 = unsafe { 580i32 as *mut i32 };
-#[no_mangle]
-pub static mut cpl: *mut u8 = unsafe { 612i32 as *mut u8 };
-#[no_mangle]
-pub static mut in_hlt: *mut bool = unsafe { 616i32 as *mut bool };
-#[no_mangle]
-pub static mut last_virt_eip: *mut i32 = unsafe { 620i32 as *mut i32 };
-#[no_mangle]
-pub static mut eip_phys: *mut i32 = unsafe { 624i32 as *mut i32 };
-#[no_mangle]
-pub static mut last_virt_esp: *mut i32 = unsafe { 628i32 as *mut i32 };
-#[no_mangle]
-pub static mut esp_phys: *mut i32 = unsafe { 632i32 as *mut i32 };
-#[no_mangle]
-pub static mut sysenter_cs: *mut i32 = unsafe { 636i32 as *mut i32 };
-#[no_mangle]
-pub static mut sysenter_esp: *mut i32 = unsafe { 640i32 as *mut i32 };
-#[no_mangle]
-pub static mut sysenter_eip: *mut i32 = unsafe { 644i32 as *mut i32 };
-#[no_mangle]
-pub static mut prefixes: *mut u8 = unsafe { 648i32 as *mut u8 };
-#[no_mangle]
-pub static mut timestamp_counter: *mut u32 = unsafe { 664i32 as *mut u32 };
-#[no_mangle]
-pub static mut sreg: *mut u16 = unsafe { 668i32 as *mut u16 };
-#[no_mangle]
-pub static mut dreg: *mut i32 = unsafe { 684i32 as *mut i32 };
-#[no_mangle]
-pub static mut fw_value: *mut i32 = unsafe { 720i32 as *mut i32 };
-#[no_mangle]
-pub static mut segment_is_null: *mut bool = unsafe { 724i32 as *mut bool };
-#[no_mangle]
-pub static mut segment_offsets: *mut i32 = unsafe { 736i32 as *mut i32 };
-#[no_mangle]
-pub static mut segment_limits: *mut u32 = unsafe { 768i32 as *mut u32 };
-#[no_mangle]
-pub static mut protected_mode: *mut bool = unsafe { 800i32 as *mut bool };
-#[no_mangle]
-pub static mut is_32: *mut bool = unsafe { 804i32 as *mut bool };
-#[no_mangle]
-pub static mut stack_size_32: *mut bool = unsafe { 808i32 as *mut bool };
-#[no_mangle]
-pub static mut memory_size: *mut u32 = unsafe { 812i32 as *mut u32 };
-#[no_mangle]
-pub static mut fpu_stack_empty: *mut i32 = unsafe { 816i32 as *mut i32 };
-#[no_mangle]
-pub static mut mxcsr: *mut i32 = unsafe { 824i32 as *mut i32 };
-#[no_mangle]
-pub static mut reg_xmm: *mut reg128 = unsafe { 828i32 as *mut reg128 };
-#[no_mangle]
-pub static mut current_tsc: *mut u64 = unsafe { 956i32 as *mut u64 };
-#[no_mangle]
-pub static mut fpu_st: *mut f64 = unsafe { 968i32 as *mut f64 };
-#[no_mangle]
-pub static mut fpu_st8: *mut u8 = unsafe { 968i32 as *mut u8 };
-#[no_mangle]
-pub static mut fpu_st32: *mut i32 = unsafe { 968i32 as *mut i32 };
-#[no_mangle]
-pub static mut fpu_stack_ptr: *mut u32 = unsafe { 1032i32 as *mut u32 };
-#[no_mangle]
-pub static mut fpu_control_word: *mut i32 = unsafe { 1036i32 as *mut i32 };
-#[no_mangle]
-pub static mut fpu_status_word: *mut i32 = unsafe { 1040i32 as *mut i32 };
-#[no_mangle]
-pub static mut fpu_opcode: *mut i32 = unsafe { 1044i32 as *mut i32 };
-#[no_mangle]
-pub static mut fpu_ip: *mut i32 = unsafe { 1048i32 as *mut i32 };
-#[no_mangle]
-pub static mut fpu_ip_selector: *mut i32 = unsafe { 1052i32 as *mut i32 };
-#[no_mangle]
-pub static mut fpu_dp: *mut i32 = unsafe { 1056i32 as *mut i32 };
-#[no_mangle]
-pub static mut fpu_dp_selector: *mut i32 = unsafe { 1060i32 as *mut i32 };
-#[no_mangle]
-pub static mut reg_mmx: *mut reg64 = unsafe { 1064i32 as *mut reg64 };
-#[no_mangle]
-pub static mut opstats_buffer: *mut u32 = unsafe { 4096i32 as *mut u32 };
-#[no_mangle]
-pub static mut opstats_buffer_0f: *mut u32 = unsafe { 5120i32 as *mut u32 };
-#[no_mangle]
-pub static mut tlb_data: *mut i32 = unsafe { 4194304i32 as *mut i32 };
+pub const reg8: *mut u8 = 4i32 as *mut u8;
+pub const reg16: *mut u16 = 4i32 as *mut u16;
+pub const reg8s: *mut i8 = 4i32 as *mut i8;
+pub const reg16s: *mut i16 = 4i32 as *mut i16;
+pub const reg32s: *mut i32 = 4i32 as *mut i32;
+pub const last_op1: *mut i32 = 512i32 as *mut i32;
+pub const last_op2: *mut i32 = 516i32 as *mut i32;
+pub const last_op_size: *mut i32 = 520i32 as *mut i32;
+pub const last_add_result: *mut i32 = 524i32 as *mut i32;
+pub const last_result: *mut i32 = 528i32 as *mut i32;
+pub const flags_changed: *mut i32 = 532i32 as *mut i32;
+pub const flags: *mut i32 = 536i32 as *mut i32;
+pub const page_fault: *mut bool = 540i32 as *mut bool;
+pub const a20_enabled: *mut bool = 552i32 as *mut bool;
+pub const instruction_pointer: *mut i32 = 556i32 as *mut i32;
+pub const previous_ip: *mut i32 = 560i32 as *mut i32;
+pub const idtr_size: *mut i32 = 564i32 as *mut i32;
+pub const idtr_offset: *mut i32 = 568i32 as *mut i32;
+pub const gdtr_size: *mut i32 = 572i32 as *mut i32;
+pub const gdtr_offset: *mut i32 = 576i32 as *mut i32;
+pub const cr: *mut i32 = 580i32 as *mut i32;
+pub const cpl: *mut u8 = 612i32 as *mut u8;
+pub const in_hlt: *mut bool = 616i32 as *mut bool;
+pub const last_virt_eip: *mut i32 = 620i32 as *mut i32;
+pub const eip_phys: *mut i32 = 624i32 as *mut i32;
+pub const last_virt_esp: *mut i32 = 628i32 as *mut i32;
+pub const esp_phys: *mut i32 = 632i32 as *mut i32;
+pub const sysenter_cs: *mut i32 = 636i32 as *mut i32;
+pub const sysenter_esp: *mut i32 = 640i32 as *mut i32;
+pub const sysenter_eip: *mut i32 = 644i32 as *mut i32;
+pub const prefixes: *mut u8 = 648i32 as *mut u8;
+pub const timestamp_counter: *mut u32 = 664i32 as *mut u32;
+pub const sreg: *mut u16 = 668i32 as *mut u16;
+pub const dreg: *mut i32 = 684i32 as *mut i32;
+pub const fw_value: *mut i32 = 720i32 as *mut i32;
+pub const segment_is_null: *mut bool = 724i32 as *mut bool;
+pub const segment_offsets: *mut i32 = 736i32 as *mut i32;
+pub const segment_limits: *mut u32 = 768i32 as *mut u32;
+pub const protected_mode: *mut bool = 800i32 as *mut bool;
+pub const is_32: *mut bool = 804i32 as *mut bool;
+pub const stack_size_32: *mut bool = 808i32 as *mut bool;
+pub const memory_size: *mut u32 = 812i32 as *mut u32;
+pub const fpu_stack_empty: *mut i32 = 816i32 as *mut i32;
+pub const mxcsr: *mut i32 = 824i32 as *mut i32;
+pub const reg_xmm: *mut reg128 = 828i32 as *mut reg128;
+pub const current_tsc: *mut u64 = 956i32 as *mut u64;
+pub const fpu_st: *mut f64 = 968i32 as *mut f64;
+pub const fpu_st8: *mut u8 = 968i32 as *mut u8;
+pub const fpu_st32: *mut i32 = 968i32 as *mut i32;
+pub const fpu_stack_ptr: *mut u32 = 1032i32 as *mut u32;
+pub const fpu_control_word: *mut i32 = 1036i32 as *mut i32;
+pub const fpu_status_word: *mut i32 = 1040i32 as *mut i32;
+pub const fpu_opcode: *mut i32 = 1044i32 as *mut i32;
+pub const fpu_ip: *mut i32 = 1048i32 as *mut i32;
+pub const fpu_ip_selector: *mut i32 = 1052i32 as *mut i32;
+pub const fpu_dp: *mut i32 = 1056i32 as *mut i32;
+pub const fpu_dp_selector: *mut i32 = 1060i32 as *mut i32;
+pub const reg_mmx: *mut reg64 = 1064i32 as *mut reg64;
+pub const opstats_buffer: *mut u32 = 4096i32 as *mut u32;
+pub const opstats_buffer_0f: *mut u32 = 5120i32 as *mut u32;
+pub const tlb_data: *mut i32 = 4194304i32 as *mut i32;
