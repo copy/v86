a0c984dc8dad81eeb3f0e587366fe81aaf94e3cf
Ernest Wong <sudo@ernestwong.nz>
2018-09-22 20:44:08 +1200

    Filesystem: Introduce IndexedDBFileStorage
    Uses MemoryFileStorage as a fallback when:
    - v86 is used in a browserless environment, e.g. NodeJS
    - browser doesn't support indexedDB
    - opening indexedDB fails
    - existing database is a newer version
    - existing database is an old version and is under use - blocking
    current v86 instance to connect to it.

--- src/browser/filestorage.js
+++ src/browser/filestorage.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const INDEXEDDB_STORAGE_VERSION = 1;
+const INDEXEDDB_STORAGE_NAME = "IndexedDBFileStorage";
+const INDEXEDDB_STORAGE_STORE = "Store";
 
 /** @interface */
 function FileStorage() {}
@@ -91,5 +94,154 @@ MemoryFileStorage.prototype.set = function(sha256sum, buffer)
     {
         this.filedata.set(sha256sum, buffer);
         resolve();
+    });
+};
+
+/**
+ * @constructor
+ * @implements {FileStorage}
+ * @param {string=} baseurl
+ */
+function IndexedDBFileStorage(baseurl)
+{
+    this.fallback_storage = new MemoryFileStorage(baseurl);
+    this.baseurl = baseurl;
+    this.db = null;
+
+    if(typeof indexedDB === "undefined")
+    {
+        dbg_log("IndexedDB not available. Using MemoryFileStorage as fallback.", LOG_9P);
+    }
+    else
+    {
+        const open_request = indexedDB.open(INDEXEDDB_STORAGE_NAME, INDEXEDDB_STORAGE_VERSION);
+
+        open_request.onblocked = event =>
+        {
+            dbg_log("IndexedDB blocked by an older database version being opened.", LOG_9P);
+            dbg_log("Using MemoryFileStorage until unblocked.", LOG_9P);
+        };
+
+        open_request.onerror = event =>
+        {
+            dbg_log("Error opening IndexedDB! Are you in private browsing mode? ", LOG_9P);
+            dbg_log("Falling back to MemoryFileStorage. Error: " + open_request.error, LOG_9P);
+        };
+
+        open_request.onupgradeneeded = event =>
+        {
+            const db = open_request.result;
+            db.createObjectStore(INDEXEDDB_STORAGE_STORE, { keyPath: "sha256sum" });
+        };
+
+        open_request.onsuccess = event =>
+        {
+            // Fallback no longer needed.
+            this.fallback_storage = null;
+
+            this.db = open_request.result;
+            this.db.onabort = event =>
+            {
+                dbg_assert(false, "IndexedDBFileStorage: transaction aborted unexpectedly");
+            };
+            this.db.onclose = event =>
+            {
+                dbg_assert(false, "IndexedDBFileStorage: connection closed unexpectedly");
+            };
+            this.db.onerror = error =>
+            {
+                dbg_assert(false,  "IndexedDBFileStorage: unexpected error: " + error);
+            };
+            this.db.onversionchange = event =>
+            {
+                dbg_log("Warning: another v86 instance is trying to open IndexedDB database but " +
+                    "is blocked by this current v86 instance.", LOG_9P);
+            };
+        };
+    }
+}
+
+/**
+ * @private
+ * @param {string} sha256sum
+ * @return {!Promise<Uint8Array>}
+ */
+IndexedDBFileStorage.prototype.load_from_server = function(sha256sum)
+{
+    dbg_assert(this.db, "IndexedDBFileStorage load_from_server called without opening database");
+
+    return new Promise((resolve, reject) =>
+    {
+        if(!this.baseurl)
+        {
+            resolve(null);
+            return;
+        }
+        v86util.load_file(this.baseurl + sha256sum, { done: buffer =>
+        {
+            const data = new Uint8Array(buffer);
+            const transaction = this.db.transaction(INDEXEDDB_STORAGE_STORE, "readwrite");
+            const store = transaction.objectStore(INDEXEDDB_STORAGE_STORE);
+            const request = store.put({ sha256sum, data });
+            request.onsuccess = event => resolve(data);
+        }});
+    });
+};
+
+/**
+ * @param {string} sha256sum
+ * @return {!Promise<Uint8Array>}
+ */
+IndexedDBFileStorage.prototype.get = function(sha256sum)
+{
+    dbg_assert(sha256sum !== "", "IndexedDBFileStorage get: sha256sum should not be an empty string");
+
+    if(!this.db)
+    {
+        return this.fallback_storage.get(sha256sum);
+    }
+
+    return new Promise((resolve, reject) =>
+    {
+        const transaction = this.db.transaction(INDEXEDDB_STORAGE_STORE, "readonly");
+        const store = transaction.objectStore(INDEXEDDB_STORAGE_STORE);
+        const request = store.get(sha256sum);
+        request.onsuccess = event =>
+        {
+            dbg_assert(!request.result || request.result.data instanceof Uint8Array,
+                "IndexedDBFileStorage get: invalid entry format: " + request.result);
+
+            if(request.result && request.result.data instanceof Uint8Array)
+            {
+                resolve(request.result.data);
+            }
+            else
+            {
+                this.load_from_server(sha256sum).then(data => resolve(data));
+            }
+        };
+    });
+};
+
+/**
+ * @param {string} sha256sum
+ * @param {!Uint8Array} data
+ * @return {!Promise}
+ */
+IndexedDBFileStorage.prototype.set = function(sha256sum, data)
+{
+    dbg_assert(sha256sum !== "", "IndexedDBFileStorage set: sha256sum should not be an empty string");
+
+    if(!this.db)
+    {
+        return this.fallback_storage.get(sha256sum);
+    }
+
+    return new Promise((resolve, reject) =>
+    {
+        const transaction = this.db.transaction(INDEXEDDB_STORAGE_STORE, "readwrite");
+        const store = transaction.objectStore(INDEXEDDB_STORAGE_STORE);
+        const request = store.put({ sha256sum, data });
+        request.onsuccess = event => resolve();
     });
 };
--- src/browser/starter.js
+++ src/browser/starter.js
@@ -565,7 +565,10 @@ function V86Starter(options)
         var fs_url = options["filesystem"]["basefs"];
         var base_url = options["filesystem"]["baseurl"];
 
-        this.fs9p = new FS(new MemoryFileStorage(base_url));
+        const fileStorage = typeof indexedDB === "undefined" ?
+            new MemoryFileStorage(base_url) :
+            new IndexedDBFileStorage(base_url);
+        this.fs9p = new FS(fileStorage);
         settings.fs9p = this.fs9p;
 
         if(fs_url)
@@ -1099,7 +1102,10 @@ V86Starter.prototype.serial0_send = function(data)
  */
 V86Starter.prototype.mount_fs = function(path, baseurl, basefs, callback)
 {
-    const newfs = new FS(new MemoryFileStorage(base_url), this.fs9p.qidcounter);
+    const fileStorage = typeof indexedDB === "undefined" ?
+        new MemoryFileStorage(baseurl) :
+        new IndexedDBFileStorage(baseurl);
+    const newfs = new FS(fileStorage, this.fs9p.qidcounter);
     const mount = () =>
     {
         const idx = this.fs9p.Mount(path, newfs);
