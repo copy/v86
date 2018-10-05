58695b1ddbdf5e6310281bc82c94be0a8b974346
Fabian <copy@copy.sh>
2018-10-05 17:43:50 -0300

    Quote variables properly

--- docker/debian-full/create-9p-from-container.sh
+++ docker/debian-full/create-9p-from-container.sh
@@ -12,16 +12,16 @@ OUT_ROOTFS_FLAT=$(dirname "$0")/../../images/debian-9p-rootfs-flat
 OUT_FSJSON=$(dirname "$0")/../../images/debian-base-fs.json
 CONTAINER_NAME=debian-full
 
-rm -rf $OUT_ROOTFS/ && mkdir $OUT_ROOTFS/
-docker export $CONTAINER_NAME | tar -xvC $OUT_ROOTFS/
+rm -rf "$OUT_ROOTFS/" && mkdir "$OUT_ROOTFS/"
+docker export "$CONTAINER_NAME" | tar -xvC "$OUT_ROOTFS/"
 
-$(dirname "$0")/../../tools/fs2json.py --out $OUT_FSJSON $OUT_ROOTFS/
-chmod 644 $OUT_FSJSON
+$(dirname "$0")/../../tools/fs2json.py --out "$OUT_FSJSON" "$OUT_ROOTFS/"
+chmod 644 "$OUT_FSJSON"
 
 # Note: Not deleting old files here
-$(dirname "$0")/../../tools/copy-to-sha256.py $OUT_ROOTFS $OUT_ROOTFS_FLAT
+$(dirname "$0")/../../tools/copy-to-sha256.py "$OUT_ROOTFS" "$OUT_ROOTFS_FLAT"
 
 #find $OUT_ROOTFS/ -type d -exec chmod 755 {} ";"
 #find $OUT_ROOTFS/ -type f -exec chmod 644 {} ";"
 
-echo $OUT_ROOTFS and $OUT_ROOTFS_FLAT created.
+echo "$OUT_ROOTFS" and "$OUT_ROOTFS_FLAT" created.
