4bb3c14e57b03163374e7df6f0c0b5a8642a4bd7
Fabian <copy@copy.sh>
2020-12-31 19:14:28 -0600

    Comment

--- gen/generate_analyzer.js
+++ gen/generate_analyzer.js
@@ -217,6 +217,7 @@ function gen_instruction_body_after_fixed_g(encoding, size)
     const imm_read = gen_read_imm_call(encoding, size);
     const instruction_postfix = [];
 
+    // jump_offset_imm: Is a block boundary, but gets a different type (Jump) below
     if(encoding.block_boundary && !encoding.jump_offset_imm || (!encoding.custom && encoding.e))
     {
         instruction_postfix.push("analysis.ty = ::analysis::AnalysisType::BlockBoundary;");
