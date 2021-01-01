6643dd14fd3cff348f2a9ebe084246f5d04b98b7
Fabian <copy@copy.sh>
2020-12-31 19:14:33 -0600

    Remove remains of old js cpu code

--- src/log.js
+++ src/log.js
@@ -94,7 +94,7 @@ function dbg_trace(level)
 {
     if(!DEBUG) return;
 
-    dbg_log(Error().stack.replace(/(?:(?:t|t16|t32)\.\(anonymous function\)\.)+/g, "t.(anonymous function)."), level);
+    dbg_log(Error().stack, level);
 }
 
 /**
