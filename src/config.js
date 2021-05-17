f3e79ae6bebec0c01ab4a0cefa7183e217b16139
Fabian <copy@copy.sh>
2021-05-17 18:36:19 -0500

    Less verbose logging

--- src/config.js
+++ src/config.js
@@ -35,7 +35,7 @@ var DUMP_UNCOMPILED_ASSEMBLY = false;
 var TRACK_FILENAMES = false;
 
 var LOG_LEVEL = LOG_ALL & ~LOG_PS2 & ~LOG_PIT & ~LOG_VIRTIO & ~LOG_9P & ~LOG_PIC &
-                          ~LOG_DMA & ~LOG_SERIAL & ~LOG_NET & ~LOG_FLOPPY & ~LOG_DISK & ~LOG_VGA;
+                          ~LOG_DMA & ~LOG_SERIAL & ~LOG_NET & ~LOG_FLOPPY & ~LOG_DISK & ~LOG_VGA & ~LOG_SB16;
 
 /**
  * @const
