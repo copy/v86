c4237fbeb8a35c85ae735aecbac593e70ae841fd
Ryan <69303239+spetterman66@users.noreply.github.com>
2023-09-02 08:20:07 +0200

    Update instructions_0f.rs
    heh, was just a little typo then lol

--- src/rust/cpu/instructions_0f.rs
+++ src/rust/cpu/instructions_0f.rs
@@ -585,7 +585,7 @@ pub unsafe fn instr_F30F12(source: reg128, r: i32) {
     };
     write_xmm_reg128(r, result);
 }
-pub unsafe fn instr_FF30F12_reg(r1: i32, r2: i32) { instr_F30F12(read_xmm128s(r1), r2); }
+pub unsafe fn instr_F30F12_reg(r1: i32, r2: i32) { instr_F30F12(read_xmm128s(r1), r2); }
 pub unsafe fn instr_F30F12_mem(addr: i32, r: i32) {
     instr_F30F12(return_on_pagefault!(safe_read128s(addr)), r);
 }
