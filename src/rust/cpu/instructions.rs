9d7ddba3938c3b10191fbdad311c2dacd239f546
Ryan <69303239+spetterman66@users.noreply.github.com>
2023-09-04 12:07:27 +0200

    Update instructions.rs

--- src/rust/cpu/instructions.rs
+++ src/rust/cpu/instructions.rs
@@ -2000,6 +2000,7 @@ pub unsafe fn instr_DD_1_reg(r: i32) {
 #[no_mangle]
 pub unsafe fn instr_DD_1_mem(addr: i32) { fpu_fisttplm16(addr) }
 pub unsafe fn instr_DF_0_mem(addr: i32) { fpu_fildm16(addr) }
+pub unsafe fn instr_DF_1_mem(addr: i32) { fpu_fxchm16(addr) }
 pub unsafe fn instr_DF_mem(addr: i32) { fpu_fisttpm16(addr) }
 pub unsafe fn instr_DF_2_mem(addr: i32) { fpu_fistm16(addr); }
 pub unsafe fn instr_DF_3_mem(addr: i32) { fpu_fistm16p(addr); }
