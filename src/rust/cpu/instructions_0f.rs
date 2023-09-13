d901901d6cffa2634a477d592d66e5f9b4ebe13e
Ryan <69303239+spetterman66@users.noreply.github.com>
2023-09-13 12:55:43 +0200

    Update instructions_0f.rs

--- src/rust/cpu/instructions_0f.rs
+++ src/rust/cpu/instructions_0f.rs
@@ -574,11 +574,9 @@ pub unsafe fn instr_F30F12(source: reg128, r: i32) {
     // movsldup xmm1, xmm2/m128
     let destination = read_xmm128s(r);
     let result = reg128 {
-        f32: [
-            destination.f32[0] + destination.f32[1],
-            destination.f32[2] + destination.f32[3],
-            source.f32[0] + source.f32[1],
-            source.f32[2] + source.f32[3],
+        f64: [
+            source.f64[1] + destination.f64[2]
+            source.f64[2] + destination.f64[1]
         ],
     };
     write_xmm_reg128(r, result);
@@ -688,8 +686,8 @@ pub unsafe fn instr_F30F16(source: reg128, r: i32) {
     let destination = read_xmm128s(r);
     let result = reg128 {
         f64: [
-            destination.f64[0] + source.f64[1],
-            destination.f64[1] + source.f64[1],
+            source.f64[1] + destination.f64[2]
+            source.f64[2] + destination.f64[1]
         ],
     };
     write_xmm_reg128(r, result);
