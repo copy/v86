8e531f5d08e8dd35786493f8d0a097343a32d5b6
Ryan <69303239+spetterman66@users.noreply.github.com>
2023-09-14 18:59:38 +0200

    Update instructions_0f.rs

--- src/rust/cpu/instructions_0f.rs
+++ src/rust/cpu/instructions_0f.rs
@@ -557,8 +557,8 @@ pub unsafe fn instr_F20F12(source: reg128, r: i32) {
     let destination = read_xmm128s(r);
     let result = reg128 {
         f64: [
-            destination.f64[0] := source.f64[0],
-            destination.f64[1] := source.f64[0],
+            destination.f64[0] = source.f64[0],
+            destination.f64[1] = source.f64[0],
         ],
     };
     write_xmm_reg128(r, result);
