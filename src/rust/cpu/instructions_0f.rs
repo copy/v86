f868ecc0374842212310d5660bce2dbb5fadc593
Ryan <69303239+spetterman66@users.noreply.github.com>
2023-09-15 14:56:37 +0200

    Update instructions_0f.rs

--- src/rust/cpu/instructions_0f.rs
+++ src/rust/cpu/instructions_0f.rs
@@ -556,9 +556,9 @@ pub unsafe fn instr_F20F12(source: reg128, r: i32) {
     // movddup xmm1, xmm2/m64
     movh_r128_m64(addr, r);
 }
-pub unsafe fn instr_F20F12_reg(r1: reg128, r2: i32) { instr_F20F12(read_reg128(r1), r2); }
-pub unsafe fn instr_F20F12_mem(addr: reg128, r: i32) {
-    instr_F20F12(return_on_pagefault!(safe_read32s(addr)), r);
+pub unsafe fn instr_F20F12_reg(r1: i32, r: i32) { instr_F20F12(read_xmm64s(r1), r); }
+pub unsafe fn instr_F20F12_mem(addr: i32, r: i32) {
+    instr_F2F012(return_on_pagefault!(safe_read64s(addr)), r)
 }
 #[no_mangle]
 pub unsafe fn instr_F30F12(source: f32, r: i32) {
