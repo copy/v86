3ec7d4af592998a07a337ee15b328b28a2a40856
Fabian <copy@copy.sh>
2023-09-16 13:51:48 -0500

    fsave: mask fpu cs/opcode (amd)

--- tests/nasm/fsave_frstor.asm
+++ tests/nasm/fsave_frstor.asm
@@ -8,5 +8,6 @@ global _start
     fsave [esp]
     frstor [esp]
     mov dword [esp + 12], 0 ; fpu eip (currently not emulated)
+    mov dword [esp + 16], 0 ; fpu cs/opcode (currently not emulated)
 
 %include "footer.inc"
