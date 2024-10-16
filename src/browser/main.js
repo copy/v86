bccf45b3da32a7b8402f2e0ade72085e157d2e2c
Fabian <copy@copy.sh>
2024-10-16 14:17:09 -0600

    proxy url presets

--- src/browser/main.js
+++ src/browser/main.js
@@ -1454,6 +1454,19 @@
                     start_emulation(profile, query_args);
                 });
         }
+
+        function set_proxy_value(id, value)
+        {
+            const elem = $(id);
+            if(elem)
+            {
+                elem.onclick = () => $("relay_url").value = value;
+            }
+        }
+        set_proxy_value("network_none", "");
+        set_proxy_value("network_fetch", "fetch");
+        set_proxy_value("network_relay", "wss://relay.widgetry.org/");
+        set_proxy_value("network_wisp", "wisps://wisp.mercurywork.shop/v86/");
     }
 
     function debug_onload()
