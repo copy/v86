f66e39cd89d446f533928ffbbb8e3b8d09a89dee
Christian Schnell <cs@rekoba.de>
2024-12-01 10:40:09 +0100

    moved fetch URL decoding from starter.js into main.js

--- src/browser/starter.js
+++ src/browser/starter.js
@@ -311,12 +311,9 @@ V86.prototype.continue_init = async function(emulator, options)
     if(relay_url)
     {
         // TODO: remove bus, use direct calls instead
-        if(relay_url.startsWith("fetch")) {
-            const fetch_options = {};
-            if(relay_url.length > 6 && relay_url[5] === ":") {
-                fetch_options.cors_proxy = relay_url.slice(6);
-            }
-            this.network_adapter = new FetchNetworkAdapter(this.bus, fetch_options);
+        if(relay_url === "fetch")
+        {
+            this.network_adapter = new FetchNetworkAdapter(this.bus, options.net_device ? options.net_device : {});
         }
         else if(relay_url.startsWith("wisp://") || relay_url.startsWith("wisps://"))
         {
