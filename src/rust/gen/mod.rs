2d154f3727c4e85a7eb471702d5c30fc15b87385
Fabian <copy@copy.sh>
2025-02-20 20:42:55 +0100

    fix rustfmt stupidity

--- src/rust/gen/mod.rs
+++ src/rust/gen/mod.rs
@@ -1,8 +1,14 @@
+#[rustfmt::skip]
 pub mod interpreter;
+#[rustfmt::skip]
 pub mod interpreter0f;
 
+#[rustfmt::skip]
 pub mod jit;
+#[rustfmt::skip]
 pub mod jit0f;
 
+#[rustfmt::skip]
 pub mod analyzer;
+#[rustfmt::skip]
 pub mod analyzer0f;
