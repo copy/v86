1d25373c498eb2361c9b6eaa45b6f53f1e8d5cbc
Rob Blanckaert <basicer@basicer.com>
2025-03-29 01:26:10 -0700

    debug run.js

--- tests/kvm-unit-tests/run.mjs
+++ tests/kvm-unit-tests/run.mjs
@@ -11,7 +11,7 @@ process.on("unhandledRejection", exn => { throw exn; });
 
 const TEST_RELEASE_BUILD = +process.env.TEST_RELEASE_BUILD;
 
-var { V86 } = await import(`../../build/${TEST_RELEASE_BUILD ? "libv86" : "libv86-debug"}.js`);
+var { V86 } = (await import(`../../build/${TEST_RELEASE_BUILD ? "libv86" : "libv86-debug"}.js`)).default;
 
 function readfile(path)
 {
