6ad46985221b2dd43c72a2aa66fdc1da0dd0df02
Oleg A. Arkhangelsky <sysoleg@yandex.ru>
2025-10-13 17:27:13 +0000

    serial: Improve websocket buffering and latency

--- src/browser/serial_websocket.js
+++ src/browser/serial_websocket.js
@@ -1,6 +1,8 @@
 import { LOG_SERIAL } from "../const.js";
 import { dbg_log } from "../log.js";
 
+import { BusConnector } from "../bus.js";
+
 /**
  * @constructor
  * @param {string} url
@@ -12,7 +14,10 @@ export function SerialAdapterWebSocket(url, bus) {
     this.socket = null;
 
     this.byteBuffer = [];
-    this.bufferInterval = null;
+
+    this.pendingFlushTimeout = null;
+    this.flushDelayMs = 10;
+    this.lastFlushTime = performance.now();
 
     this.init();
 }
@@ -22,16 +27,63 @@ SerialAdapterWebSocket.prototype.init = function() {
 
     this.bus.register("serial0-output-byte", (byte) => {
         this.byteBuffer.push(byte);
-        if (this.byteBuffer.length >= 500) {
+
+        const now = performance.now();
+
+        // If we already have a pending trailing flush, let it fire; don't force immediate flush
+        if (this.pendingFlushTimeout !== null) {
+            return;
+        }
+
+        // If this is the first byte in the buffer, prefer scheduling a trailing flush
+        if (this.byteBuffer.length === 1) {
+            this.maybeScheduleFlush(now, { deferIfDue: true });
+            return;
+        }
+
+        // If enough time has passed since last flush, flush immediately
+        if (now - this.lastFlushTime >= this.flushDelayMs) {
             this.flushBuffer();
+        } else {
+            // Otherwise, schedule a trailing flush
+            this.maybeScheduleFlush(now, {});
         }
     }, this);
 };
 
+SerialAdapterWebSocket.prototype.maybeScheduleFlush = function(now, opts) {
+    if (this.pendingFlushTimeout !== null) return;
+
+    const options = opts || {};
+    const elapsed = now - this.lastFlushTime;
+    let remaining = this.flushDelayMs - elapsed;
+
+    if (remaining <= 0) {
+        if (options.deferIfDue) {
+            // Defer slightly to allow more bytes to join, but keep latency tiny
+            remaining = 2;
+        } else {
+            this.flushBuffer();
+            return;
+        }
+    }
+
+    this.pendingFlushTimeout = setTimeout(() => {
+        this.pendingFlushTimeout = null;
+        this.flushBuffer();
+    }, remaining);
+};
+
 SerialAdapterWebSocket.prototype.flushBuffer = function() {
+    if (this.pendingFlushTimeout !== null) {
+        clearTimeout(this.pendingFlushTimeout);
+        this.pendingFlushTimeout = null;
+    }
+
     if (this.byteBuffer.length > 0 && this.socket && this.socket.readyState === WebSocket.OPEN) {
         this.socket.send(new Uint8Array(this.byteBuffer));
         this.byteBuffer = [];
+        this.lastFlushTime = performance.now();
     }
 };
 
@@ -41,11 +93,11 @@ SerialAdapterWebSocket.prototype.connect = function() {
 
     this.socket.onopen = () => {
         dbg_log("WebSocket serial adapter connected", LOG_SERIAL);
-        this.bufferInterval = setInterval(() => this.flushBuffer(), 10);
+        this.lastFlushTime = performance.now();
     };
 
-    this.socket.onmessage = async (event) => {
-        const data = new Uint8Array(event.data);
+    this.socket.onmessage = (event) => {
+        const data = new Uint8Array(/** @type {ArrayBuffer} */ (event.data));
         for (let i = 0; i < data.length; i++) {
             this.bus.send("serial0-input", data[i]);
         }
@@ -53,9 +105,9 @@ SerialAdapterWebSocket.prototype.connect = function() {
 
     this.socket.onclose = () => {
         dbg_log("WebSocket serial adapter disconnected, reconnecting...", LOG_SERIAL);
-        if (this.bufferInterval) {
-            clearInterval(this.bufferInterval);
-            this.bufferInterval = null;
+        if (this.pendingFlushTimeout !== null) {
+            clearTimeout(this.pendingFlushTimeout);
+            this.pendingFlushTimeout = null;
         }
         setTimeout(() => this.connect(), 5000);
     };
@@ -66,9 +118,13 @@ SerialAdapterWebSocket.prototype.connect = function() {
 };
 
 SerialAdapterWebSocket.prototype.destroy = function() {
-    if (this.bufferInterval) {
-        clearInterval(this.bufferInterval);
+    if (this.pendingFlushTimeout !== null) {
+        clearTimeout(this.pendingFlushTimeout);
+        this.pendingFlushTimeout = null;
     }
+
+    this.flushBuffer();
+
     if (this.socket) {
         this.socket.onclose = null; // Don't reconnect
         this.socket.close();
