e48e96ab3b372149066bd401b6b915916661bf0d
Elisha Hollander <just4now666666@gmail.com>
2026-05-15 20:07:51 +0300

    calculate `offset` relative to `cs`

--- src/rust/cpu/eip_increment.rs
+++ src/rust/cpu/eip_increment.rs
@@ -1,7 +1,7 @@
-pub fn increment_instruction_pointer(eip: i32, delta: i32, is_asize_32: bool) -> i32 {
+pub fn increment_instruction_pointer(eip: i32, delta: i32, is_asize_32: bool, cs: i32) -> i32 {
     if !is_asize_32 {
-        let offset = eip & 0xFFFF;
-        eip + (((offset.wrapping_add(delta)) & 0xFFFF) - offset)
+        let offset = eip.wrapping_sub(cs) & 0xFFFF;
+        eip.wrapping_add((offset.wrapping_add(delta) & 0xFFFF) - offset)
     }
     else {
         eip.wrapping_add(delta)
