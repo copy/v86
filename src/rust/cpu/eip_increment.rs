461e4d5b21b8ba4b326777714f899f4429f8e37a
Elisha Hollander <just4now666666@gmail.com>
2026-05-22 16:07:08 +0300

    Delete src/rust/cpu/eip_increment.rs

--- src/rust/cpu/eip_increment.rs
+++
@@ -1,9 +0,0 @@
-pub fn increment_instruction_pointer(eip: i32, delta: i32, is_asize_32: bool, cs: i32) -> i32 {
-    if !is_asize_32 {
-        let offset = eip.wrapping_sub(cs) & 0xFFFF;
-        eip.wrapping_add((offset.wrapping_add(delta) & 0xFFFF) - offset)
-    }
-    else {
-        eip.wrapping_add(delta)
-    }
-}
