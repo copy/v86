f18a55bdf59c4850809b8631b778932f933e2099
Yanshu Wang <wangyenshu@outlook.com>
2026-06-22 18:50:17 +0800

    add debian xfce setup example

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -59,7 +59,7 @@ export default [
             "no-extra-semi": "error",
             "no-tabs": "error",
             "id-match": ["error",
-                "^([a-z][a-z0-9_]*|[A-Z][a-zA-Z0-9_]*|_|\\$|__dirname|__filename|spawnSync|execFile|createServer|workerData|parentPort|isMainThread|fileURLToPath|registerProcessor|sampleRate)$",
+                "^([a-z][a-z0-9_]*|[A-Z][a-zA-Z0-9_]*|_|\\$|__dirname|__filename|spawnSync|execFile|execFileSync|createServer|workerData|parentPort|isMainThread|fileURLToPath|registerProcessor|sampleRate)$",
                 { "onlyDeclarations": true, "properties": false }],
             "quotes": ["error", "double", { "avoidEscape": true, "allowTemplateLiterals": true }],
             "keyword-spacing": ["error", { "overrides": {
